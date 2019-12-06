24820e222296a6d23f30cab011cccf10665cac0b
nabeelamirch <namir@companieshouse.gov.uk>
2019-12-06 13:53:25 +0000

    added NODE_END=production

--- start.sh
+++ start.sh
@@ -29,5 +29,5 @@ else
     source "${APP_DIR}/global_env"
     source "${APP_DIR}/app_env"
 
-    exec node ${APP_DIR}/bin/www.js -- $PORT
+    exec NODE_ENV=production node ${APP_DIR}/bin/www.js -- $PORT
 fi
