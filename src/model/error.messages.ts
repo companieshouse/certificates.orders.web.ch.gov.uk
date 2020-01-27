8a4411e207aa1908b42e34764ee54e1ba3b0a25c
ethatcher83 <ethatcher@companieshouse.gov.uk>
2020-01-27 07:32:09 +0000

    Add order details to router Add order details controller Add order details web template Add order details unit tests Update links for journey flow

--- src/model/error.messages.ts
+++ src/model/error.messages.ts
@@ -1,3 +1,7 @@
 export const ERROR_SUMMARY_TITLE: string = "This page has errors";
 
 export const GOOD_STANDING_OPTION_NOT_SELECTED: string = "Select if you want good standing information on the certificate";
+export const ORDERS_DETAILS_FIRST_NAME_EMPTY: string = "Enter your first name";
+export const ORDERS_DETAILS_LAST_NAME_EMPTY: string = "Enter your last name";
+export const ORDER_DETAILS_NAME_MAX_LENGTH: string = "First name must be 32 characters or fewer";
+export const INVALID_CHARACTERS: string = "You have entered invalid characters";
---
+++ src/model/template.paths.ts
@@ -0,0 +1,3 @@
+export const INDEX: string = "index";
+export const GOOD_STANDING: string = "good-standing";
+export const ORDER_DETAILS: string = "order-details";
