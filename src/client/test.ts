db9eb6dd8ceecca56b1c8f59139909b3bf0089c6
nabeelamirch <namir@companieshouse.gov.uk>
2020-03-20 15:33:02 +0000

    added type for certificate

--- src/client/test.ts
+++ src/client/test.ts
@@ -1,11 +1,13 @@
 import {createApiClient} from "ch-sdk-node";
+import Resource from "ch-sdk-node/dist/services/resource";
+import {CertificateItem} from "ch-sdk-node/dist/services/order/item/certificate";
 
 (async () => {
-    const api = createApiClient(undefined, "rs2YV0Q0F8BKfgXM17yhyxkOpRCH0PFGmFTUxhfhW_J7PheKQXJL0HQmagHY-nCGjWrr0amt6tUqe0-drEleHQ", "http://api.chs-dev.internal:4001");
+    const api = createApiClient(undefined, "AnnEPL4YhQc5TyzZB9-9Ze_LtpPQCARqh0QbOi46Da1ZlznDGvElOPeYFrcHekStNaJjqj2epF0pZhWXQSuyXA", "http://api.chs-dev.internal:4001");
     const sdkResponse =
         await api.companyProfile.getCompanyProfile("00006400");
         //console.log(sdkResponse);
-    const profile = await api.certificateItem.getCertificateItem("CHS00000000000000001");
+    const profile: Resource<CertificateItem> = await api.certificate.getCertificate("CHS00000000000000001");
 
     console.log(profile);
 })();
