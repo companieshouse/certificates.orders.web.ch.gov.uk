1655b8b48b4883284cca16f7698390683e5eb94a
ethatcher83 <ethatcher@companieshouse.gov.uk>
2020-03-20 11:58:48 +0000

    updated test file

--- src/client/test.ts
+++ src/client/test.ts
@@ -1,7 +1,10 @@
 import {createApiClient} from "ch-sdk-node";
 
 (async () => {
-    const api = createApiClient("F0v8XszSNH4R2-CdM1ZozNaNIrXUvAyZuOvkHm6cZYTdcG4RnlFL6m50FNDysccklk4u9884Tsjx14EHgQrpfg");
+    const api = createApiClient(undefined, "rs2YV0Q0F8BKfgXM17yhyxkOpRCH0PFGmFTUxhfhW_J7PheKQXJL0HQmagHY-nCGjWrr0amt6tUqe0-drEleHQ", "http://api.chs-dev.internal:4001");
+    const sdkResponse =
+        await api.companyProfile.getCompanyProfile("00006400");
+        //console.log(sdkResponse);
     const profile = await api.certificateItem.getCertificateItem("CHS00000000000000001");
 
     console.log(profile);
