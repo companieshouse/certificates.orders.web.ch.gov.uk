3f442867f9ac8b2f15d79d8dd82e9fc48a31b80e
nabeelamirch <namir@companieshouse.gov.uk>
2020-03-23 15:33:01 +0000

    added post request to test file

--- src/client/test.ts
+++ src/client/test.ts
@@ -1,13 +1,18 @@
 import {createApiClient} from "ch-sdk-node";
 import Resource from "ch-sdk-node/dist/services/resource";
-import {CertificateItem} from "ch-sdk-node/dist/services/order/item/certificate";
+import {CertificateItem, CertificateItemRequest} from "ch-sdk-node/dist/services/order/item/certificate";
 
 (async () => {
-    const api = createApiClient(undefined, "AnnEPL4YhQc5TyzZB9-9Ze_LtpPQCARqh0QbOi46Da1ZlznDGvElOPeYFrcHekStNaJjqj2epF0pZhWXQSuyXA", "http://api.chs-dev.internal:4001");
-    const sdkResponse =
-        await api.companyProfile.getCompanyProfile("00006400");
-        //console.log(sdkResponse);
-    const profile: Resource<CertificateItem> = await api.certificate.getCertificate("CHS00000000000000001");
+    const api = createApiClient(undefined, "5MiYQ0o9VIIQxsF9yl49Ws75Lnq-3ZCc9Cx4_iVB49c5xsXpRdt2mSgegQ7WACUNGDz4baGrr7HIESoa7hPS_g", "http://api.chs-dev.internal:4001");
+    const sdkResponse = await api.companyProfile.getCompanyProfile("00006400");
+    //const cert: Resource<CertificateItem> = await api.certificate.getCertificate("CHS00000000000000001");
+    //console.log(JSON.stringify(cert, null, 2));
 
-    console.log(profile);
+    const c: CertificateItemRequest = {
+        companyNumber: "00006400",
+        itemOptions: {},
+        quantity: 1,
+    };
+    const createdCert: Resource<CertificateItem> = await api.certificate.postCertificate(c);
+    console.log(JSON.stringify(createdCert, null, 2));
 })();
