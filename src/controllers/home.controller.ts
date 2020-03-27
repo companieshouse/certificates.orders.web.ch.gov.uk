1692e4228a86106f641e544ff612a064e826731b
nabeelamirch <namir@companieshouse.gov.uk>
2020-03-27 16:33:13 +0000

    removed unnecessary files

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,7 +1,7 @@
 import {Request, Response} from "express";
 import {ORDER_DETAILS, replaceCompanyNumber} from "../model/page.urls";
 
-export default async (req: Request, res: Response) => {
+export default (req: Request, res: Response) => {
   const companyNumber: string = req.params.companyNumber;
   const orderDetailsUrl = replaceCompanyNumber(ORDER_DETAILS, companyNumber);
   res.render("index", {orderDetailsUrl, companyNumber});
