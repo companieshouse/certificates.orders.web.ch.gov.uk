d905afc7040ab1b4fbdce70b21795228b9cb5071
nabeelamirch <namir@companieshouse.gov.uk>
2020-04-15 15:27:02 +0100

    formatted tests

--- src/test/controllers/certificate.options.spec.integration.ts
+++ src/test/controllers/certificate.options.spec.integration.ts
@@ -1,7 +1,7 @@
 import {createRedisMock, getSignedInCookie} from "../utils/mock.redis"
 jest.mock('ioredis', () => createRedisMock());
 
-import { postCertificateItem } from '../../client/api.client';
+import {postCertificateItem} from '../../client/api.client';
 jest.mock('../../client/api.client');
 
 import app from "../../app";
@@ -12,30 +12,30 @@ const mockPostCertificateItem: jest.Mock = (<unknown>postCertificateItem as jest
 
 describe("certificate options controller", () => {
 
-  describe("certificate options get", () => {
-    it("renders the certificate options page", async () => {
-      const resp = await request(app)
-          .get(CERTIFICATE_OPTIONS)
-          .set('Cookie', [getSignedInCookie()]);;
-      
-      expect(resp.status).toEqual(200);
-      expect(resp.text).toContain("What information would you like to be included on the certificate?");
-    });
-  });
+    describe("certificate options get", () => {
+        it("renders the certificate options page", async () => {
+            const resp = await request(app)
+                .get(CERTIFICATE_OPTIONS)
+                .set('Cookie', [getSignedInCookie()]);;
 
-  describe("certificate options post", () => {
-    it("redirects the user to the order-details page", async () => {
-      mockPostCertificateItem.mockImplementation(() => {});
-      const resp = await request(app)
-          .post(CERTIFICATE_OPTIONS)
-          .send({
-            'more-info': ["good-standing", "registered-office"],
-          })
-          .set('Cookie', [getSignedInCookie()]);;
-      
-      expect(resp.status).toEqual(302);
-      expect(resp.text).toContain("Found. Redirecting to order-details");
+            expect(resp.status).toEqual(200);
+            expect(resp.text).toContain("What information would you like to be included on the certificate?");
+        });
     });
-  });
+
+    describe("certificate options post", () => {
+        it("redirects the user to the order-details page", async () => {
+            mockPostCertificateItem.mockImplementation(() => { });
+            const resp = await request(app)
+                .post(CERTIFICATE_OPTIONS)
+                .send({
+                    'more-info': ["good-standing", "registered-office"],
+                })
+                .set('Cookie', [getSignedInCookie()]);;
+
+            expect(resp.status).toEqual(302);
+            expect(resp.text).toContain("Found. Redirecting to order-details");
+        });
+    });
 
 });
--- src/test/controllers/certificate.options.spec.unit.ts
+++ src/test/controllers/certificate.options.spec.unit.ts
@@ -2,61 +2,61 @@ import {setItemOptions} from "../../controllers/certificate.options.controller";
 
 describe("setItemOptions", () => {
 
-  it("should set includeBasicInformation on DirectorDetails to true, when the option is directors", () => {
-    const options = ["directors"];
-    const returnedItemOptions = setItemOptions(options);
+    it("should set includeBasicInformation on DirectorDetails to true, when the option is directors", () => {
+        const options = ["directors"];
+        const returnedItemOptions = setItemOptions(options);
 
-    expect(returnedItemOptions?.directorDetails?.includeBasicInformation).toBeTruthy();
-  });
+        expect(returnedItemOptions?.directorDetails?.includeBasicInformation).toBeTruthy();
+    });
 
-  it("should set includeCompanyObjectsInformation to true, when the option is company-objects", () => {
-    const options = ["company-objects"];
-    const returnedItemOptions = setItemOptions(options);
+    it("should set includeCompanyObjectsInformation to true, when the option is company-objects", () => {
+        const options = ["company-objects"];
+        const returnedItemOptions = setItemOptions(options);
 
-    expect(returnedItemOptions?.includeCompanyObjectsInformation).toBeTruthy();
-  });
+        expect(returnedItemOptions?.includeCompanyObjectsInformation).toBeTruthy();
+    });
 
-  it("should set includeGoodStandingInformation to true, when the option is good-standing", () => {
-    const options = ["good-standing"];
-    const returnedItemOptions = setItemOptions(options);
+    it("should set includeGoodStandingInformation to true, when the option is good-standing", () => {
+        const options = ["good-standing"];
+        const returnedItemOptions = setItemOptions(options);
 
-    expect(returnedItemOptions?.includeGoodStandingInformation).toBeTruthy();
-  });
+        expect(returnedItemOptions?.includeGoodStandingInformation).toBeTruthy();
+    });
 
-  it("should set includeAddressRecordsType on registeredOfficeAddressDetails to current, when the option is registered-office", () => {
-    const options = ["registered-office"];
-    const returnedItemOptions = setItemOptions(options);
+    it("should set includeAddressRecordsType on registeredOfficeAddressDetails to current, when the option is registered-office", () => {
+        const options = ["registered-office"];
+        const returnedItemOptions = setItemOptions(options);
 
-    expect(returnedItemOptions?.registeredOfficeAddressDetails?.includeAddressRecordsType).toBe("current");
-  });
+        expect(returnedItemOptions?.registeredOfficeAddressDetails?.includeAddressRecordsType).toBe("current");
+    });
 
-  it("should set includeBasicInformation on secretaryDetails to true, when the option is secretaries", () => {
-    const options = ["secretaries"];
-    const returnedItemOptions = setItemOptions(options);
+    it("should set includeBasicInformation on secretaryDetails to true, when the option is secretaries", () => {
+        const options = ["secretaries"];
+        const returnedItemOptions = setItemOptions(options);
 
-    expect(returnedItemOptions?.secretaryDetails?.includeBasicInformation).toBeTruthy();
-  });
+        expect(returnedItemOptions?.secretaryDetails?.includeBasicInformation).toBeTruthy();
+    });
 
-  it("should set multiple itemOptions, when multiple options are set", () => {
-    const options = ["secretaries", "good-standing", "company-objects"];
-    const returnedItemOptions = setItemOptions(options);
+    it("should set multiple itemOptions, when multiple options are set", () => {
+        const options = ["secretaries", "good-standing", "company-objects"];
+        const returnedItemOptions = setItemOptions(options);
 
-    expect(returnedItemOptions?.secretaryDetails?.includeBasicInformation).toBeTruthy();
-    expect(returnedItemOptions?.includeGoodStandingInformation).toBeTruthy();
-    expect(returnedItemOptions?.includeCompanyObjectsInformation).toBeTruthy();
-    expect(returnedItemOptions?.directorDetails?.includeBasicInformation).toBeNull();
-    expect(returnedItemOptions?.registeredOfficeAddressDetails?.includeAddressRecordsType).toBeNull();
-  });
+        expect(returnedItemOptions?.secretaryDetails?.includeBasicInformation).toBeTruthy();
+        expect(returnedItemOptions?.includeGoodStandingInformation).toBeTruthy();
+        expect(returnedItemOptions?.includeCompanyObjectsInformation).toBeTruthy();
+        expect(returnedItemOptions?.directorDetails?.includeBasicInformation).toBeNull();
+        expect(returnedItemOptions?.registeredOfficeAddressDetails?.includeAddressRecordsType).toBeNull();
+    });
 
-  it("should leave multiple itemOptions to null, when there are no options", () => {
-    const options = [];
-    const returnedItemOptions = setItemOptions(options);
+    it("should leave multiple itemOptions to null, when there are no options", () => {
+        const options = [];
+        const returnedItemOptions = setItemOptions(options);
 
-    expect(returnedItemOptions?.secretaryDetails?.includeBasicInformation).toBeNull();
-    expect(returnedItemOptions?.includeGoodStandingInformation).toBeNull();
-    expect(returnedItemOptions?.includeCompanyObjectsInformation).toBeNull();
-    expect(returnedItemOptions?.directorDetails?.includeBasicInformation).toBeNull();
-    expect(returnedItemOptions?.registeredOfficeAddressDetails?.includeAddressRecordsType).toBeNull();
-  });
+        expect(returnedItemOptions?.secretaryDetails?.includeBasicInformation).toBeNull();
+        expect(returnedItemOptions?.includeGoodStandingInformation).toBeNull();
+        expect(returnedItemOptions?.includeCompanyObjectsInformation).toBeNull();
+        expect(returnedItemOptions?.directorDetails?.includeBasicInformation).toBeNull();
+        expect(returnedItemOptions?.registeredOfficeAddressDetails?.includeAddressRecordsType).toBeNull();
+    });
 
 });
