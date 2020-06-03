c34690bc39195acb858a1ee9f8d35946fb306e88
nabeelamirch <namir@companieshouse.gov.uk>
2020-06-03 14:37:23 +0100

    removed jest tests and dependencies

--- src/test/setup.ts
+++
@@ -1,8 +0,0 @@
-export default async () => {
-  process.env.PIWIK_URL = "test";
-  process.env.PIWIK_SITE_ID = "test";
-  process.env.COOKIE_SECRET = "Xy6onkjQWF0TkRn0hfdqUw==";
-  process.env.CACHE_SERVER = "secret";
-  process.env.API_URL = "api url";
-  process.env.CHS_URL = "chs url";
-};
