44f7810d864d79a4bcd68b64767f08e830e93efa
mwejuli <mwejuli@companieshouse.gov.uk>
2020-06-16 09:09:57 +0100

    chore: remove unused imports

--- src/controllers/certified-copies/home.controller.ts
+++ src/controllers/certified-copies/home.controller.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from "express";
-// import { CHS_URL, APPLICATION_NAME } from "../config/config";
 import { CERTIFIFIED_COPY_FILING_HISTORY, replaceCompanyNumber } from "../../model/page.urls";
 
 export default (req: Request, res: Response) => {
     const companyNumber: string = req.params.companyNumber;
     const startNowUrl = replaceCompanyNumber(CERTIFIFIED_COPY_FILING_HISTORY, companyNumber);
-    res.render("index", { startNowUrl, companyNumber });
+    res.render("");
 };
