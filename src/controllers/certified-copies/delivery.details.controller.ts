4bbc098f262166225823a6bacdc5c9ce1c16eef6
mwejuli <mwejuli@companieshouse.gov.uk>
2020-06-16 09:05:09 +0100

    chore: remove CHS_URL from certified-copy controllers

--- src/controllers/certified-copies/delivery.details.controller.ts
+++ src/controllers/certified-copies/delivery.details.controller.ts
@@ -1,12 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import { createLogger } from "ch-structured-logging";
-import { CHS_URL, APPLICATION_NAME } from "../../config/config";
+import { APPLICATION_NAME } from "../../config/config";
+
 
 const logger = createLogger(APPLICATION_NAME);
 
 export const render = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-        res.render("");
+        res.render('');
     } catch (err) {
         logger.error(`${err}`);
         next(err);
