7dc923d187e9bf7e18de9f4582b73ca13c6512fd
mwejuli <mwejuli@companieshouse.gov.uk>
2020-06-16 09:55:11 +0100

    chore: lint auto fix

--- src/controllers/certified-copies/check.details.controller.ts
+++ src/controllers/certified-copies/check.details.controller.ts
@@ -2,12 +2,11 @@ import { NextFunction, Request, Response } from "express";
 import { createLogger } from "ch-structured-logging";
 import { APPLICATION_NAME } from "../../config/config";
 
-
 const logger = createLogger(APPLICATION_NAME);
 
 export const render = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-        res.render('');
+        res.render("");
     } catch (err) {
         logger.error(`${err}`);
         next(err);
