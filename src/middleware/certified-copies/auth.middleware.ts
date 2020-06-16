cdbf191b0d0e4d8e8cf94d0c85827cb53ecf4375
mwejuli <mwejuli@companieshouse.gov.uk>
2020-06-16 11:03:56 +0100

    chore: remove unused imports from router

--- src/middleware/certified-copies/auth.middleware.ts
+++ src/middleware/certified-copies/auth.middleware.ts
@@ -1,9 +1,4 @@
 import { NextFunction, Request, RequestHandler, Response } from "express";
-import { SessionKey } from "ch-node-session-handler/lib/session/keys/SessionKey";
-import { SignInInfoKeys } from "ch-node-session-handler/lib/session/keys/SignInInfoKeys";
-
-// import { getCertificateItem } from "../../client/api.client";
-import { getAccessToken } from "../../session/helper";
 import { createLogger } from "ch-structured-logging";
 
 import { APPLICATION_NAME } from "../../config/config";
