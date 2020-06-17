bd479feb9859ada11f8afc685bdf5585c59475b9
mwejuli <mwejuli@companieshouse.gov.uk>
2020-06-17 13:07:54 +0100

    chore: use local config variable

--- src/controllers/certified-copies/home.controller.ts
+++ src/controllers/certified-copies/home.controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import { CERTIFIFIED_COPY_FILING_HISTORY, replaceCompanyNumber } from "../../model/page.urls";
 import { CERTIFIED_COPY_INDEX } from "../../model/template.paths";
+import { CHS_URL } from "../../config/config";
 
 export default (req: Request, res: Response) => {
     const companyNumber: string = req.params.companyNumber;
-    const startNowUrl = `${process.env.CHS_URL}${replaceCompanyNumber(CERTIFIFIED_COPY_FILING_HISTORY, companyNumber)}`;
+    const startNowUrl = `${CHS_URL}${replaceCompanyNumber(CERTIFIFIED_COPY_FILING_HISTORY, companyNumber)}`;
     res.render(CERTIFIED_COPY_INDEX, { startNowUrl, companyNumber });
 };
