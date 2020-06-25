1a6a3341b362014677c292f8ba835a010e8f03b7
mwejuli <mwejuli@companieshouse.gov.uk>
2020-06-25 13:31:27 +0100

    feature: add service names to config

--- src/controllers/certified-copies/home.controller.ts
+++ src/controllers/certified-copies/home.controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
 import { CERTIFIED_COPY_FILING_HISTORY, replaceCompanyNumber } from "../../model/page.urls";
 import { CERTIFIED_COPY_INDEX } from "../../model/template.paths";
-import { CHS_URL } from "../../config/config";
+import { CHS_URL, SERVICE_NAME_CERTIFIED_COPIES } from "../../config/config";
 
 export default (req: Request, res: Response) => {
     const companyNumber: string = req.params.companyNumber;
     const startNowUrl = `${CHS_URL}${replaceCompanyNumber(CERTIFIED_COPY_FILING_HISTORY, companyNumber)}`;
-    const serviceName: string = "Order a certified document";
+    const serviceName: string = SERVICE_NAME_CERTIFIED_COPIES;
     res.render(CERTIFIED_COPY_INDEX, { startNowUrl, companyNumber, serviceName });
 };
