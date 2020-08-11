6b1808327ab5f2c6bc1de34b10476e0bf08f5eae
ethatcher83 <ethatcher@companieshouse.gov.uk>
2020-08-11 10:41:59 +0100

    update test

--- test/controller/certified-copies/home.controller.integration.test.ts
+++ test/controller/certified-copies/home.controller.integration.test.ts
@@ -2,8 +2,6 @@ import chai from "chai";
 import sinon from "sinon";
 import ioredis from "ioredis";
 import { ROOT_CERTIFIED_COPY, replaceCompanyNumber } from "../../../src/model/page.urls";
-import { CompanyProfile } from "ch-sdk-node/dist/services/company-profile";
-import Resource from "ch-sdk-node/dist/services/resource";
 import CompanyProfileService from "ch-sdk-node/dist/services/company-profile/service";
 
 const COMPANY_NUMBER = "00000000";
@@ -13,52 +11,50 @@ let testApp = null;
 let getCompanyProfileStub;
 let dummyCompanyProfile;
 
-const dummyCompanyProfileNoFilingHistory: Resource<CompanyProfile> = {
-    httpStatusCode: 200,
-    resource: {
-        companyName: "company name",
-        companyNumber: "00000000",
-        companyStatus: "active",
-        companyStatusDetail: "company status detail",
-        dateOfCreation: "date of creation",
-        jurisdiction: "jurisdiction",
-        sicCodes: ["85100", "85200"],
-        hasBeenLiquidated: false,
-        type: "other",
-        hasCharges: false,
-        hasInsolvencyHistory: false,
-        registeredOfficeAddress: {
-            addressLineOne: "line1",
-            addressLineTwo: "line2",
-            careOf: "careOf",
-            country: "uk",
-            locality: "locality",
-            poBox: "123",
-            postalCode: "post code",
-            premises: "premises",
-            region: "region"
-        },
-        accounts: {
-            nextAccounts: {
-                periodEndOn: "2019-10-10",
-                periodStartOn: "2019-01-01"
-            },
-            nextDue: "2020-05-31",
-            overdue: false
-        },
-        confirmationStatement: {
-            nextDue: "2020-05-31",
-            overdue: false
-        },
-        links: {
-        }
-    }
-};
-
 describe("certified-copy.home.controller.integration", () => {
     beforeEach((done) => {
         sandbox.stub(ioredis.prototype, "connect").returns(Promise.resolve());
-        dummyCompanyProfile = dummyCompanyProfileNoFilingHistory;
+        dummyCompanyProfile = {
+            httpStatusCode: 200,
+            resource: {
+                companyName: "company name",
+                companyNumber: "00000000",
+                companyStatus: "active",
+                companyStatusDetail: "company status detail",
+                dateOfCreation: "date of creation",
+                jurisdiction: "jurisdiction",
+                sicCodes: ["85100", "85200"],
+                hasBeenLiquidated: false,
+                type: "other",
+                hasCharges: false,
+                hasInsolvencyHistory: false,
+                registeredOfficeAddress: {
+                    addressLineOne: "line1",
+                    addressLineTwo: "line2",
+                    careOf: "careOf",
+                    country: "uk",
+                    locality: "locality",
+                    poBox: "123",
+                    postalCode: "post code",
+                    premises: "premises",
+                    region: "region"
+                },
+                accounts: {
+                    nextAccounts: {
+                        periodEndOn: "2019-10-10",
+                        periodStartOn: "2019-01-01"
+                    },
+                    nextDue: "2020-05-31",
+                    overdue: false
+                },
+                confirmationStatement: {
+                    nextDue: "2020-05-31",
+                    overdue: false
+                },
+                links: {
+                }
+            }
+        };
 
         testApp = require("../../../src/app").default;
         done();
@@ -67,17 +63,6 @@ describe("certified-copy.home.controller.integration", () => {
     afterEach(() => {
         sandbox.reset();
         sandbox.restore();
-    });
-
-    it("does not render the start now page as company has no filing history link", async () => {
-        getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
-            .returns(Promise.resolve(dummyCompanyProfile));
-
-        const resp = await chai.request(testApp)
-            .get(replaceCompanyNumber(ROOT_CERTIFIED_COPY, COMPANY_NUMBER));
-
-        chai.expect(resp.status).to.equal(200);
-        chai.expect(resp.text).to.contain("You cannot order a certificate or certified document for this company. ");
     });
 
     it("renders the start page as company has a filing history link", async () => {
@@ -90,6 +75,17 @@ describe("certified-copy.home.controller.integration", () => {
 
         chai.expect(resp.status).to.equal(200);
         chai.expect(resp.text).to.contain("Order a certified document");
+    });
+
+    it("does not render the start now page as company has no filing history link", async () => {
+        getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
+            .returns(Promise.resolve(dummyCompanyProfile));
+
+        const resp = await chai.request(testApp)
+            .get(replaceCompanyNumber(ROOT_CERTIFIED_COPY, COMPANY_NUMBER));
+
+        chai.expect(resp.status).to.equal(200);
+        chai.expect(resp.text).to.contain("You cannot order a certificate or certified document for this company. ");
     });
 
     it("does not render the start now page if company type is uk establishment and has a filing history link", async () => {
