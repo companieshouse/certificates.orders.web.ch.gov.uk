b518f5f7d3944dacd06433cee63bc3b2e226365a
nabeelamirch <namir@companieshouse.gov.uk>
2020-08-13 09:52:15 +0100

    added unit test and updated ch-sdk-node

--- test/config/api.enumerations.unit.test.ts
+++ test/config/api.enumerations.unit.test.ts
@@ -8,5 +8,10 @@ describe("api.enumerations.unit", () => {
             const result = getFullFilingHistoryDescription("change-person-director-company-with-change-date");
             expect(result).to.equal("**Director's details changed** for {officer_name} on {change_date}");
         });
+
+        it("should return the same input passed in if filing history description is not present", () => {
+            const result = getFullFilingHistoryDescription("test-123");
+            expect(result).to.equal("test-123");
+        });
     });
 });
