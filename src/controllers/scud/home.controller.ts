e3f2427ccf2142e8b4746591c617e51414b92200
caismith-ch <csmith2@companieshouse.gov.uk>
2020-09-08 08:39:26 +0100

    Fix introduced npm lint issues

--- src/controllers/scud/home.controller.ts
+++ src/controllers/scud/home.controller.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 import { SCAN_UPON_DEMAND_INDEX } from "../../model/template.paths";
-import { SCAN_UPON_DEMAND_CREATE, replaceCompanyNumber} from "../../model/page.urls";
+import { SCAN_UPON_DEMAND_CREATE, replaceCompanyNumber } from "../../model/page.urls";
 
-export default async(req: Request, res: Response) => {
+export default async (req: Request, res: Response) => {
     const companyNumber: string = req.params.companyNumber;
     const startNowUrl: string = replaceCompanyNumber(SCAN_UPON_DEMAND_CREATE, companyNumber);
 
