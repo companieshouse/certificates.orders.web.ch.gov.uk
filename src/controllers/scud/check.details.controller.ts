a89d93e0af85ea17d72ea7685e2a97aa33a69ebc
caismith-ch <csmith2@companieshouse.gov.uk>
2020-09-11 08:25:48 +0100

    Commit code to help find issue causing "Session object missing"

--- src/controllers/scud/check.details.controller.ts
+++ src/controllers/scud/check.details.controller.ts
@@ -1,7 +1,20 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { SCAN_UPON_DEMAND_CHECK_DETAILS } from "../../model/template.paths";
+import { getAccessToken, getUserId } from "../../session/helper";
+import { APPLICATION_NAME } from "../../config/config";
+import { createLogger } from "ch-structured-logging";
 
-export default async (req: Request, res: Response) => {
-    console.log("Hello");
-    res.render(SCAN_UPON_DEMAND_CHECK_DETAILS);
+const logger = createLogger(APPLICATION_NAME);
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const userId = getUserId(req.session);
+        const accessToken: string = getAccessToken(req.session);
+        
+        console.log("You have hit the check details controller, Rendering: " + SCAN_UPON_DEMAND_CHECK_DETAILS);
+        return res.render(SCAN_UPON_DEMAND_CHECK_DETAILS);
+    } catch (err) {
+        logger.error(`${err}`);
+        next(err);
+    }
 };
--- src/controllers/scud/create.scud.item.controller.ts
+++ src/controllers/scud/create.scud.item.controller.ts
@@ -1,6 +1,20 @@
-import { Request, Response } from "express";
-import { SCAN_UPON_DEMAND_CHECK_DETAILS } from "../../model/template.paths";
+import { NextFunction, Request, Response } from "express";
+import { SCAN_UPON_DEMAND_CHECK_DETAILS } from "../../model/page.urls";
+import { createLogger } from "ch-structured-logging";
+import { APPLICATION_NAME } from "../../config/config";
+import { getAccessToken, getUserId } from "../../session/helper";
 
-export default async (req: Request, res: Response) => {
-    res.redirect(SCAN_UPON_DEMAND_CHECK_DETAILS);
+const logger = createLogger(APPLICATION_NAME);
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const userId = getUserId(req.session);
+        const accessToken: string = getAccessToken(req.session);
+
+        console.log("You have hit the SCUD controller - Redirecting: " + SCAN_UPON_DEMAND_CHECK_DETAILS);
+        res.redirect(SCAN_UPON_DEMAND_CHECK_DETAILS);
+    } catch (err) {
+        logger.error(`${err}`);
+        next(err);
+    }
 };
