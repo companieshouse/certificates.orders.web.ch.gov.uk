388e3a3cc5342e6b46de40b2c54060504ae1fb7f
nabeelamirch <namir@companieshouse.gov.uk>
2020-10-05 09:40:03 +0100

    add mid to basket and redirect to orders web

--- src/routers/missing-image-delivery/routers.ts
+++ src/routers/missing-image-delivery/routers.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { ROOT_MISSING_IMAGE_DELIVERY, MISSING_IMAGE_DELIVERY_CHECK_DETAILS, MISSING_IMAGE_DELIVERY_CREATE } from "../../model/page.urls";
 import homeController from "../../controllers/missing-image-delivery/home.controller";
 import { render as renderCreateController } from "../../controllers/missing-image-delivery/create.missing.image.delivery.item.controller";
-import checkDetailsController from "../../controllers/missing-image-delivery/check.details.controller";
+import checkDetailsController, { render as renderCheckDetails } from "../../controllers/missing-image-delivery/check.details.controller";
 
 const router: Router = Router();
 
 router.get(ROOT_MISSING_IMAGE_DELIVERY, homeController);
 router.get(MISSING_IMAGE_DELIVERY_CREATE, renderCreateController);
-router.get(MISSING_IMAGE_DELIVERY_CHECK_DETAILS, checkDetailsController);
+router.get(MISSING_IMAGE_DELIVERY_CHECK_DETAILS, renderCheckDetails);
+router.post(MISSING_IMAGE_DELIVERY_CHECK_DETAILS, checkDetailsController);
 
 export default router;
