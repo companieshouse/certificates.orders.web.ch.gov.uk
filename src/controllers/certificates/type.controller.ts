21446ed919aee6506d429f084fe8213ce7aab467
ethatcher83 <ethatcher@companieshouse.gov.uk>
2020-10-08 15:38:57 +0100

    add creation of dissolved certificate within type controller add type controller integration test refactor company profile mocks into seperate file for test rename scud test to missing image delivery

--- src/controllers/certificates/type.controller.ts
+++ src/controllers/certificates/type.controller.ts
@@ -2,10 +2,11 @@ import { Request, Response, NextFunction } from "express";
 
 import { getAccessToken, getUserId } from "../../session/helper";
 import { CertificateItemPostRequest, CertificateItem } from "ch-sdk-node/dist/services/order/certificates/types";
-import { postCertificateItem } from "../../client/api.client";
-import { CERTIFICATE_OPTIONS, replaceCertificateId } from "./../../model/page.urls";
+import { postCertificateItem, getCompanyProfile } from "../../client/api.client";
+import { CERTIFICATE_OPTIONS, replaceCertificateId, CERTIFICATE_DELIVERY_DETAILS } from "./../../model/page.urls";
 import { createLogger } from "ch-structured-logging";
-import { APPLICATION_NAME } from "../../config/config";
+import { APPLICATION_NAME, API_KEY } from "../../config/config";
+import { CompanyProfile } from "ch-sdk-node/dist/services/company-profile/types";
 
 const logger = createLogger(APPLICATION_NAME);
 
@@ -13,20 +14,39 @@ export const render = async (req: Request, res: Response, next: NextFunction): P
     try {
         const accessToken: string = getAccessToken(req.session);
         const companyNumber = req.params.companyNumber;
-
-        const certificateItemRequest: CertificateItemPostRequest = {
-            companyNumber,
-            itemOptions: {
-                certificateType: "incorporation-with-all-name-changes",
-                deliveryMethod: "postal",
-                deliveryTimescale: "standard"
-            },
-            quantity: 1
-        };
-        const userId = getUserId(req.session);
-        const certificateItem: CertificateItem = await postCertificateItem(accessToken, certificateItemRequest);
-        logger.info(`Certificate Item created, id=${certificateItem.id}, user_id=${userId}, company_number=${certificateItem.companyNumber}`);
-        res.redirect(replaceCertificateId(CERTIFICATE_OPTIONS, certificateItem.id));
+        const companyProfile: CompanyProfile = await getCompanyProfile(API_KEY, companyNumber);
+        const companyStatus = companyProfile.companyStatus;
+       
+        if (companyStatus === "active") {
+            const certificateItemRequest: CertificateItemPostRequest = {
+                companyNumber,
+                itemOptions: {
+                    certificateType: "incorporation-with-all-name-changes",
+                    deliveryMethod: "postal",
+                    deliveryTimescale: "standard"
+                },
+                quantity: 1
+            };
+        
+            const userId = getUserId(req.session);
+            const certificateItem: CertificateItem = await postCertificateItem(accessToken, certificateItemRequest);
+            logger.info(`Certificate Item created, id=${certificateItem.id}, user_id=${userId}, company_number=${certificateItem.companyNumber}`);
+            res.redirect(replaceCertificateId(CERTIFICATE_OPTIONS, certificateItem.id));
+        } else {
+            const certificateItemRequest: CertificateItemPostRequest = {
+                companyNumber,
+                itemOptions: {
+                    certificateType: "dissolution",
+                    deliveryMethod: "postal",
+                    deliveryTimescale: "standard"
+                },
+                quantity: 1
+            };
+            const userId = getUserId(req.session);
+            const certificateItem: CertificateItem = await postCertificateItem(accessToken, certificateItemRequest);
+            logger.info(`Dissolved certificate Item created, id=${certificateItem.id}, user_id=${userId}, company_number=${certificateItem.companyNumber}`);
+            res.redirect(replaceCertificateId(CERTIFICATE_DELIVERY_DETAILS, certificateItem.id));
+        }
     } catch (err) {
         logger.error(`${err}`);
         next(err);
---
+++ test/controller/certificates/type.controller.integration.test.ts
@@ -0,0 +1,80 @@
+import chai from "chai";
+import sinon from "sinon";
+import ioredis from "ioredis";
+
+import * as apiClient from "../../../src/client/api.client";
+import { CERTIFICATE_TYPE, replaceCertificateId } from "../../../src/model/page.urls";
+import { SIGNED_IN_COOKIE, signedInSession } from "../../__mocks__/redis.mocks";
+import { CertificateItem } from "ch-sdk-node/dist/services/order/certificates/types";
+import { dummyCompanyProfileActiveCompany, dummyCompanyProfileDissolvedCompany } from "../../__mocks__/company.profile.mocks"
+
+const sandbox = sinon.createSandbox();
+let testApp = null;
+let postCertificateItemStub;
+let postDissolvedCertificateItemStub;
+let getCompanyProfileStub;
+const COMPANY_NUMBER = "00006500";
+const CERTIFICATE_TYPE_URL = replaceCertificateId(CERTIFICATE_TYPE, COMPANY_NUMBER);
+
+describe("type.controller.integration", () => {
+    beforeEach((done) => {
+        sandbox.stub(ioredis.prototype, "connect").returns(Promise.resolve());
+        sandbox.stub(ioredis.prototype, "get").returns(Promise.resolve(signedInSession));
+
+        testApp = require("../../../src/app").default;
+        done();
+    });
+
+    afterEach(() => {
+        sandbox.reset();
+        sandbox.restore();
+    });
+
+    describe("certificate item", () => {
+        it("redirects the user to the certificate options page for standard certificate", async () => {
+            const certificateDetails = {
+                id: "CRT-951616-000712",
+                itemOptions: {
+                    certificateType: "incorporation-with-all-name-changes"
+                }
+            } as CertificateItem;
+
+            postCertificateItemStub = sandbox.stub(apiClient, "postCertificateItem")
+                .returns(Promise.resolve(certificateDetails));
+            getCompanyProfileStub = sandbox.stub(apiClient, "getCompanyProfile")
+            .returns(Promise.resolve(dummyCompanyProfileActiveCompany));
+
+            const resp = await chai.request(testApp)
+                .get(CERTIFICATE_TYPE_URL)
+                .set("Cookie", [`__SID=${SIGNED_IN_COOKIE}`])
+                .redirects(0);
+
+            chai.expect(resp.status).to.equal(302);
+            chai.expect(resp.text).to.include("Found. Redirecting to /orderable/certificates/CRT-951616-000712/certificate-options");
+        });
+    });
+
+    describe("dissolved certificate item", () => {
+        it("redirects the user to the delivery details page for dissolved certificates", async () => {
+            const dissolvedCertificateDetails = {
+                id: "CRT-951616-000712",
+                itemOptions: {
+                    certificateType: "dissolution"
+                }
+            } as CertificateItem;
+
+            getCompanyProfileStub = sandbox.stub(apiClient, "getCompanyProfile")
+                .returns(Promise.resolve(dummyCompanyProfileDissolvedCompany));
+            postDissolvedCertificateItemStub = sandbox.stub(apiClient, "postCertificateItem")
+                .returns(Promise.resolve(dissolvedCertificateDetails));
+
+            const resp = await chai.request(testApp)
+                .get(CERTIFICATE_TYPE_URL)
+                .set("Cookie", [`__SID=${SIGNED_IN_COOKIE}`])
+                .redirects(0);
+
+            chai.expect(resp.status).to.equal(302);
+            chai.expect(resp.text).to.include("Found. Redirecting to /orderable/certificates/CRT-951616-000712/delivery-details");
+        });
+    });
+});
