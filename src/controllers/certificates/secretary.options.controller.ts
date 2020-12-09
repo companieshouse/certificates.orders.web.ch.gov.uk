d8f0ff29949a621a96db0c9cc1eab73764e7e283
caismith-ch <csmith2@companieshouse.gov.uk>
2020-12-09 14:01:39 +0000

    Add skeleton screen for secretaries

---
+++ src/controllers/certificates/secretary.options.controller.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import { getAccessToken } from "../../session/helper";
+import { CertificateItem } from "ch-sdk-node/dist/services/order/certificates/types";
+import { getCertificateItem } from "../../client/api.client";
+import { CERTIFICATE_SECRETARY_OPTIONS } from "../../model/template.paths";
+
+export const render = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const accessToken: string = getAccessToken(req.session);
+    const certificateItem: CertificateItem = await getCertificateItem(accessToken, req.params.certificateId);
+    const SERVICE_URL = `/company/${certificateItem.companyNumber}/orderable/certificates`;
+
+    return res.render(CERTIFICATE_SECRETARY_OPTIONS, {
+        companyNumber: certificateItem.companyNumber,
+        SERVICE_URL
+    });
+};
