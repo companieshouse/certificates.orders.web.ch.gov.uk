359d90c352d89e5f8778da9130b8bef7ce660167
caismith-ch <csmith2@companieshouse.gov.uk>
2020-12-09 14:12:48 +0000

    Add integration test for skeleton page and update title text

---
+++ test/controller/certificates/secretary.options.controller.integration.test.ts
@@ -0,0 +1,52 @@
+import chai from "chai";
+import sinon from "sinon";
+import ioredis from "ioredis";
+import { SIGNED_IN_COOKIE, signedInSession } from "../../__mocks__/redis.mocks";
+import { CertificateItem } from "ch-sdk-node/dist/services/order/certificates/types";
+import * as apiClient from "../../../src/client/api.client";
+import { CERTIFICATE_SECRETARY_OPTIONS, replaceCertificateId } from "../../../src/model/page.urls";
+
+const CERTIFICATE_ID = "CRT-000000-000000";
+const SECRETARY_OPTIONS_INFO_TEXT =
+    "We aim to include the names of all current company secretaries, along with any of the details you choose below.";
+const SECRETARY_OPTIONS_URL =
+    replaceCertificateId(CERTIFICATE_SECRETARY_OPTIONS, CERTIFICATE_ID);
+const sandbox = sinon.createSandbox();
+let testApp = null;
+let getCertificateItemStub;
+
+describe("secretary.options.integration.test", () => {
+    beforeEach((done) => {
+        sandbox.stub(ioredis.prototype, "connect").returns(Promise.resolve());
+        sandbox.stub(ioredis.prototype, "get").returns(Promise.resolve(signedInSession));
+
+        testApp = require("../../../src/app").default;
+        done();
+    });
+
+    afterEach(() => {
+        sandbox.reset();
+        sandbox.restore();
+    });
+
+    const certificateItem = {
+        itemOptions: {
+            forename: "john",
+            surname: "smith"
+        }
+    } as CertificateItem;
+
+    describe("Check the page renders", () => {
+        it("renders the secretary options page", async () => {
+            getCertificateItemStub = sandbox.stub(apiClient, "getCertificateItem")
+                .returns(Promise.resolve(certificateItem));
+
+            const resp = await chai.request(testApp)
+                .get(SECRETARY_OPTIONS_URL)
+                .set("Cookie", [`__SID=${SIGNED_IN_COOKIE}`]);
+
+            chai.expect(resp.status).to.equal(200);
+            chai.expect(resp.text).to.contain(SECRETARY_OPTIONS_INFO_TEXT);
+        });
+    });
+});
