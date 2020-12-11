1c470ab4e4f8b911392d8830cf0133e5260c2e8b
katherine-axten <kaxten@companieshouse.gov.uk>
2020-12-11 09:09:27 +0000

    Added the patching and tests

--- test/controller/certificates/director.options.controller.unit.test.ts
+++ test/controller/certificates/director.options.controller.unit.test.ts
@@ -1,7 +1,7 @@
 import chai from "chai";
 
 import { CertificateItem } from "ch-sdk-node/dist/services/order/certificates/types";
-import { setBackLink } from "../../../src/controllers/certificates/director.options.controller";
+import { setBackLink, setDirectorOption } from "../../../src/controllers/certificates/director.options.controller";
 
 describe("director.options.controller.unit", () => {
     describe("setBackUrl for no registered address option selected", () => {
@@ -28,6 +28,29 @@ describe("director.options.controller.unit", () => {
             } as CertificateItem;
 
             chai.expect(setBackLink(certificateItem)).to.equal("registered-office-options");
+        });
+    });
+
+    describe("Set includeAddress to true", () => {
+        it("when address has been ticked it should return it as true", () => {
+            const option: string [] = ["address"];
+            const returnedDirectorOption = setDirectorOption(option);
+
+            chai.expect(returnedDirectorOption.includeAddress).to.equal(true);
+        });
+    });
+
+    describe("It should set address, occupation and DOB to true when selected and everything else to false", () => {
+        it("When address, occupation and DOB have been selected, they should be set to true with everything else set to false", () => {
+            const option: string [] = ["address", "occupation", "dob"];
+            const returnedDirectorOption = setDirectorOption(option);
+
+            chai.expect(returnedDirectorOption.includeAddress).to.equal(true);
+            chai.expect(returnedDirectorOption.includeOccupation).to.equal(true);
+            chai.expect(returnedDirectorOption.includeDobType).to.equal("partial");
+            chai.expect(returnedDirectorOption.includeAppointmentDate).to.equal(false);
+            chai.expect(returnedDirectorOption.includeNationality).to.equal(false);
+            chai.expect(returnedDirectorOption.includeCountryOfResidence).to.equal(false);
         });
     });
 });
