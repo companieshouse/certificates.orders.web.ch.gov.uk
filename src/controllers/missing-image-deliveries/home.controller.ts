ea94458abefade2989a9d4d9d9b44a5c6c451da7
ethatcher83 <ethatcher@companieshouse.gov.uk>
2021-07-09 08:49:19 +0100

    Increment frontend kit package version Update home controllers to pass company name to templates Add notifiction banners to each start now page and display company name and company number Update home page integration tests

--- src/controllers/missing-image-deliveries/home.controller.ts
+++ src/controllers/missing-image-deliveries/home.controller.ts
@@ -1,12 +1,17 @@
 import { Request, Response } from "express";
 import { MISSING_IMAGE_DELIVERY_INDEX } from "../../model/template.paths";
 import { MISSING_IMAGE_DELIVERY_CREATE, replaceCompanyNumberAndFilingHistoryId } from "../../model/page.urls";
+import { CompanyProfile } from "@companieshouse/api-sdk-node/dist/services/company-profile";
+import { getCompanyProfile } from "../../client/api.client";
+import { API_KEY } from "../../config/config";
 
 export default async (req: Request, res: Response) => {
     const companyNumber: string = req.params.companyNumber;
+    const companyProfile: CompanyProfile = await getCompanyProfile(API_KEY, companyNumber);
+    const companyName : string = companyProfile.companyName;
     const filingHistoryId: string = req.params.filingHistoryId;
     const startNowUrl: string = replaceCompanyNumberAndFilingHistoryId(MISSING_IMAGE_DELIVERY_CREATE, companyNumber, filingHistoryId);
     const SERVICE_URL = `/company/${companyNumber}/filing-history`;
 
-    res.render(MISSING_IMAGE_DELIVERY_INDEX, { companyNumber, startNowUrl, SERVICE_URL });
+    res.render(MISSING_IMAGE_DELIVERY_INDEX, { companyNumber, startNowUrl, SERVICE_URL, companyName });
 };
