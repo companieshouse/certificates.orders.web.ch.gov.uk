357753d18d335322928ec580c71ce885d3adfa9f
phudson-ch <phudson@companieshouse.gov.uk>
2021-08-18 16:54:56 +0100

    Implement common landing page template for active companies
    * Refactored conditional logic for presenting the landing page into the home controller
    * Created base template for landing pages to extend
    * Created new active and dissolved landing pages that extend the base template

--- src/controllers/certificates/home.controller.ts
+++ src/controllers/certificates/home.controller.ts
@@ -32,21 +32,24 @@ export default async (req: Request, res: Response, next: NextFunction) => {
         let startNowUrl;
         let SERVICE_URL;
 
+        let view : string;
         if (companyStatus === "dissolved") {
             acceptableCompanyTypes.shift(); // Remove limited-partnership from list
             SERVICE_URL = `/company/${companyNumber}/orderable/dissolved-certificates`;
             startNowUrl = replaceCompanyNumber(DISSOLVED_CERTIFICATE_TYPE, companyNumber);
+            view = "certificates/dissolved-company-landing";
             logger.debug(`Certificates Home Controller - Dissolved Company, company_number=${companyNumber}, service_url=${SERVICE_URL}, start_now_url=${startNowUrl}`);
         } else {
             SERVICE_URL = `/company/${companyNumber}/orderable/certificates`;
             startNowUrl = replaceCompanyNumber(CERTIFICATE_TYPE, companyNumber);
+            view = "certificates/active-company-landing"
             logger.debug(`Certificates Home Controller - Active Company, company_number=${companyNumber}, service_url=${SERVICE_URL}, start_now_url=${startNowUrl}`);
         }
         const allow: boolean = acceptableCompanyTypes.some(type => type === companyType);
 
         if (allow && ["active", "dissolved"].includes(companyStatus)) {
             logger.debug(`Rendering certificates/index, company_status=${companyStatus}, start_now_url=${startNowUrl}, company_number=${companyNumber}, service_url=${SERVICE_URL}, dispatch_days=${DISPATCH_DAYS}, more_tab_url=${moreTabUrl}`);
-            res.render("certificates/index", { companyStatus, startNowUrl, companyNumber, SERVICE_URL, DISPATCH_DAYS, moreTabUrl, companyName });
+            res.render(view, { companyStatus, startNowUrl, companyNumber, SERVICE_URL, DISPATCH_DAYS, moreTabUrl, companyName });
         } else {
             const SERVICE_NAME = null;
             res.render(YOU_CANNOT_USE_THIS_SERVICE, { SERVICE_NAME });
