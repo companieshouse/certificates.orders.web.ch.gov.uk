67752ddd775f28c0f7baec66fa52e533c9c41105
phudson-ch <phudson@companieshouse.gov.uk>
2021-09-08 16:56:36 +0100

    Fix certificate page options landing page
    * Given user is not logged in ensure URL used to forward after login is company type specific
    * Given user session has expired ensure certificate options landing page is company type specific

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,14 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 import { SessionKey } from "@companieshouse/node-session-handler/lib/session/keys/SessionKey";
 import { SignInInfoKeys } from "@companieshouse/node-session-handler/lib/session/keys/SignInInfoKeys";
-import { CERTIFICATE_TYPE, replaceCompanyNumber } from "./../model/page.urls";
+import {CERTIFICATE_TYPE, LLP_CERTIFICATE_TYPE, LP_CERTIFICATE_TYPE, replaceCompanyNumber} from "./../model/page.urls";
 import { createLogger } from "ch-structured-logging";
-import { APPLICATION_NAME } from "../config/config";
+import {API_KEY, APPLICATION_NAME} from "../config/config";
 import { getUserId } from "../session/helper";
+import {CompanyProfile} from "@companieshouse/api-sdk-node/dist/services/company-profile";
+import {getCompanyProfile} from "../client/api.client";
 
 const logger = createLogger(APPLICATION_NAME);
 
-export default (req: Request, res: Response, next: NextFunction) => {
+export default async (req: Request, res: Response, next: NextFunction) => {
     try {
         if (req.path !== "/") {
             if (!req.session) {
@@ -18,7 +20,16 @@ export default (req: Request, res: Response, next: NextFunction) => {
 
             if (!signedIn) {
                 const companyNumber = req.params.companyNumber;
-                const returnToUrl = replaceCompanyNumber(CERTIFICATE_TYPE, companyNumber);
+                const companyProfile: CompanyProfile = await getCompanyProfile(API_KEY, companyNumber);
+                const companyType: string = companyProfile.type;
+                let returnToUrl: string;
+                if ('limited-partnership' === companyType) {
+                    returnToUrl = replaceCompanyNumber(LP_CERTIFICATE_TYPE, companyNumber);
+                } else if ('llp' === companyType) {
+                    returnToUrl = replaceCompanyNumber(LLP_CERTIFICATE_TYPE, companyNumber);
+                } else {
+                    returnToUrl = replaceCompanyNumber(CERTIFICATE_TYPE, companyNumber);
+                }
                 logger.info(`User unauthorized, status_code=401, redirecting to sign in page`);
                 return res.redirect(`/signin?return_to=${returnToUrl}`);
             } else {
--- src/middleware/certificates/auth.middleware.ts
+++ src/middleware/certificates/auth.middleware.ts
@@ -2,12 +2,18 @@ import { NextFunction, Request, RequestHandler, Response } from "express";
 import { SessionKey } from "@companieshouse/node-session-handler/lib/session/keys/SessionKey";
 import { SignInInfoKeys } from "@companieshouse/node-session-handler/lib/session/keys/SignInInfoKeys";
 
-import { getCertificateItem } from "../../client/api.client";
-import { CERTIFICATE_OPTIONS, replaceCertificateId, DISSOLVED_CERTIFICATE_DELIVERY_DETAILS } from "./../../model/page.urls";
+import {getCertificateItem, getCompanyProfile} from "../../client/api.client";
+import {
+    CERTIFICATE_OPTIONS,
+    replaceCertificateId,
+    DISSOLVED_CERTIFICATE_DELIVERY_DETAILS,
+    LP_CERTIFICATE_OPTIONS, LLP_CERTIFICATE_OPTIONS
+} from "./../../model/page.urls";
 import { getAccessToken } from "../../session/helper";
 import { createLogger } from "ch-structured-logging";
 
-import { APPLICATION_NAME } from "../../config/config";
+import {API_KEY, APPLICATION_NAME} from "../../config/config";
+import {CompanyProfile} from "@companieshouse/api-sdk-node/dist/services/company-profile";
 
 const logger = createLogger(APPLICATION_NAME);
 
@@ -25,6 +31,10 @@ export default async (req: Request, res: Response, next: NextFunction) => {
 
             if (originatingUrl.includes("/dissolved-certificates")) {
                 returnToUrl = replaceCertificateId(DISSOLVED_CERTIFICATE_DELIVERY_DETAILS, certificateId);
+            } else if (originatingUrl.includes("/lp-certificates")) {
+                returnToUrl = replaceCertificateId(LP_CERTIFICATE_OPTIONS, certificateId);
+            } else if (originatingUrl.includes("/llp-certificates")) {
+                returnToUrl = replaceCertificateId(LLP_CERTIFICATE_OPTIONS, certificateId);
             } else {
                 returnToUrl = replaceCertificateId(CERTIFICATE_OPTIONS, certificateId);
             }
