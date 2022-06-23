3644ec76688af87da277c8cb6be6c75b4a262a15
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2022-06-23 15:12:44 +0100

    GCI-2122: Plumb in missing MID check details auth middleware.

--- src/middleware/missing-image-deliveries/auth.middleware.ts
+++ src/middleware/missing-image-deliveries/auth.middleware.ts
@@ -1,10 +1,15 @@
-import { NextFunction, Request, RequestHandler, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { SessionKey } from "@companieshouse/node-session-handler/lib/session/keys/SessionKey";
 import { SignInInfoKeys } from "@companieshouse/node-session-handler/lib/session/keys/SignInInfoKeys";
 import { getUserId } from "../../session/helper";
-import { MISSING_IMAGE_DELIVERY_CREATE, replaceCompanyNumberAndFilingHistoryId } from "../../model/page.urls";
+import {
+    MISSING_IMAGE_DELIVERY_CHECK_DETAILS,
+    MISSING_IMAGE_DELIVERY_CREATE,
+    replaceCompanyNumberAndFilingHistoryId,
+    replaceMissingImageDeliveryId
+} from "../../model/page.urls";
 import { createLogger } from "ch-structured-logging";
-import { APPLICATION_NAME } from "../../config/config";
+import {  APPLICATION_NAME} from "../../config/config";
 import { getWhitelistedReturnToURL } from "../../utils/request.util";
 
 const logger = createLogger(APPLICATION_NAME);
@@ -21,17 +26,43 @@ export default async (req: Request, res: Response, next: NextFunction) => {
             const filingHistoryId = req.params.filingHistoryId;
             // TODO GI-2122: remove this.
             logger.info(`companyNumber = ${companyNumber}, filingHistoryId = ${filingHistoryId}, ` +
-                `missingImageDeliveryId = ${req.params.missingImageDeliveryId}, req.url = ${req.url}, req.originalUrl = ${req.originalUrl}`);
+                `req.url = ${req.url}, req.originalUrl = ${req.originalUrl}`);
             const returnToUrl = replaceCompanyNumberAndFilingHistoryId(MISSING_IMAGE_DELIVERY_CREATE, companyNumber, filingHistoryId);
             logger.info(`User unauthorized, status_code=401, redirecting to sign in page`);
             return res.redirect(`/signin?return_to=${getWhitelistedReturnToURL(returnToUrl)}`);
         } else {
             const userId = getUserId(req.session);
             logger.info(`User is signed in, user_id=${userId}`);
-        };
+        }
         next();
     } catch (err) {
         logger.error(`Missing Image Delivery authentication middleware: ${err}`);
         next(err);
-    };
+    }
+};
+
+export const authMissingImageDeliveryCheckDetailsMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        if (!req.session) {
+            logger.info(`${req.url}: Session object is missing!`);
+        }
+        const signedIn = req.session?.data?.[SessionKey.SignInInfo]?.[SignInInfoKeys.SignedIn] === 1;
+
+        if (!signedIn) {
+            const missingImageDeliveryId = req.params.missingImageDeliveryId;
+            // TODO GI-2122: remove this.
+            logger.info(`missingImageDeliveryId = ${missingImageDeliveryId}, req.url = ${req.url}, req.originalUrl = ${req.originalUrl}`);
+            const returnToUrl = replaceMissingImageDeliveryId(MISSING_IMAGE_DELIVERY_CHECK_DETAILS, missingImageDeliveryId);
+            logger.info(`User unauthorized, status_code=401, redirecting to sign in page`);
+            return res.redirect(`/signin?return_to=${getWhitelistedReturnToURL(returnToUrl)}`);
+        } else {
+            const userId = getUserId(req.session);
+            logger.info(`User is signed in, user_id=${userId}`);
+        }
+        next();
+    } catch (err) {
+        logger.error(`Missing Image Delivery authentication middleware: ${err}`);
+        next(err);
+    }
+
 };
