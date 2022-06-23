8c1fa387e12da640b6bfeda0ebb4ccbe9da179c2
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2022-06-23 10:25:30 +0100

    GCI-2122: Vet missing image delivery redirect return to URL WIP.

--- src/middleware/missing-image-deliveries/auth.middleware.ts
+++ src/middleware/missing-image-deliveries/auth.middleware.ts
@@ -5,6 +5,7 @@ import { getUserId } from "../../session/helper";
 import { MISSING_IMAGE_DELIVERY_CREATE, replaceCompanyNumberAndFilingHistoryId } from "../../model/page.urls";
 import { createLogger } from "ch-structured-logging";
 import { APPLICATION_NAME } from "../../config/config";
+import { getWhitelistedReturnToURL } from "../../utils/request.util";
 
 const logger = createLogger(APPLICATION_NAME);
 
@@ -18,9 +19,11 @@ export default async (req: Request, res: Response, next: NextFunction) => {
         if (!signedIn) {
             const companyNumber = req.params.companyNumber;
             const filingHistoryId = req.params.filingHistoryId;
+            // TODO GI-2122: remove this.
+            logger.info(`companyNumber = ${companyNumber}, filingHistoryId = ${filingHistoryId}, req.url = ${req.url}, req.originalUrl = ${req.originalUrl}`);
             const returnToUrl = replaceCompanyNumberAndFilingHistoryId(MISSING_IMAGE_DELIVERY_CREATE, companyNumber, filingHistoryId);
             logger.info(`User unauthorized, status_code=401, redirecting to sign in page`);
-            return res.redirect(`/signin?return_to=${returnToUrl}`);
+            return res.redirect(`/signin?return_to=${getWhitelistedReturnToURL(returnToUrl)}`);
         } else {
             const userId = getUserId(req.session);
             logger.info(`User is signed in, user_id=${userId}`);
