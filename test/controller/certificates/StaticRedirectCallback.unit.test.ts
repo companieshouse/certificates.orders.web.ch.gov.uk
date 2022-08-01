65c84640f8bf4973a0b71dd60958f6b481d268e2
dgroves-ch <dgroves@companieshouse.gov.uk>
2022-08-01 13:54:09 +0100

    Enrollment logic for certified certificates
    * User agent redirected to global delivery details page when
      continuing from delivery options or email options pages and no
      deliverable items in basket.
    * User agent redirected to basket page when continuing from delivery
      options or email options pages and deliverable items already in
      basket.

---
+++ test/controller/certificates/StaticRedirectCallback.unit.test.ts
@@ -0,0 +1,42 @@
+import { StaticRedirectCallback } from "../../../src/controllers/certificates/StaticRedirectCallback";
+import { expect } from "chai";
+import { createSandbox } from "sinon";
+
+describe("StaticRedirectCallback", () => {
+
+    const sandbox = createSandbox();
+
+    describe("redirectEnrolled", () => {
+        it("Redirects user agent to the basket page if predicate evaluates to true", () => {
+            // given
+            const callback = new StaticRedirectCallback(() => true);
+            const mockResponse = sandbox.spy({
+                redirect: (path: string) => {}
+            });
+            // when
+            callback.redirectEnrolled({
+                response: mockResponse,
+                items: [{}]
+            } as any);
+
+            // then
+            expect(mockResponse.redirect).to.be.calledOnceWith("/basket");
+        });
+
+        it("Redirects user agent to the delivery details page if predicate evaluates to false", () => {
+            // given
+            const callback = new StaticRedirectCallback(() => false);
+            const mockResponse = sandbox.spy({
+                redirect: (path: string) => {}
+            });
+            // when
+            callback.redirectEnrolled({
+                response: mockResponse,
+                items: [{}]
+            } as any);
+
+            // then
+            expect(mockResponse.redirect).to.be.calledOnceWith("/delivery-details");
+        });
+    });
+});
