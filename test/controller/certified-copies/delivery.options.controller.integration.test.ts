cde3541249e1fe45c3f472391f243f571c5531b9
hwilliams1 <hwilliams1@companieshouse.gov.uk>
2022-08-08 15:23:24 +0100

    Work in progress on test cases

--- test/controller/certified-copies/delivery.options.controller.integration.test.ts
+++ test/controller/certified-copies/delivery.options.controller.integration.test.ts
@@ -31,10 +31,49 @@ describe("delivery.options.controller.integration.test", () => {
     });
 
     const certifiedCopyItem = {
+        companyName: "test company",
+        companyNumber: "00000000",
+        customerReference: "reference",
+        description: "description",
+        descriptionIdentifier: "description identifier",
+        descriptionValues: {
+            key: "value"
+        },
+        etag: "etag",
+        id: "CCD-123456-123456",
+        itemCosts: [{
+            itemCost: "10",
+            productType: "type",
+            discountApplied: "0",
+            calculatedCost: "10"
+        }],
         itemOptions: {
-            forename: "john",
-            surname: "smith"
-        }
+            collectionLocation: "cardiff",
+            contactNumber: "0123456789",
+            deliveryMethod: "postal",
+            deliveryTimescale: "standard",
+            filingHistoryDocuments: [{
+                filingHistoryDate: "2010-02-12",
+                filingHistoryDescription: "change-person-director-company-with-change-date",
+                filingHistoryDescriptionValues: {
+                    change_date: "2010-02-12",
+                    officer_name: "Thomas David Wheare"
+                },
+                filingHistoryId: "MzAwOTM2MDg5OWFkaXF6a2N4",
+                filingHistoryType: "CH01",
+                filingHistoryCost: "15"
+            }],
+            forename: "forename",
+            surname: "surname"
+        },
+        links: {
+            self: "/path/to/certified-copy"
+        },
+        kind: "item#certified-copy",
+        postalDelivery: true,
+        postageCost: "0",
+        quantity: 1,
+        totalItemCost: "15"
     } as CertifiedCopyItem;
 
     describe("Check the page renders", () => {
@@ -53,20 +92,40 @@ describe("delivery.options.controller.integration.test", () => {
         });
     });
 
-    describe("delivery options validation", () => {
-        it.only("throws a validation error when no option selected", async () => {
+    describe("delivery options patch", () => {
+        it("redirects the user to the delivery-details page", async () => {
+            getCertifiedCopyItemStub = sandbox.stub(apiClient, "getCertifiedCopyItem")
+                .returns(Promise.resolve(certifiedCopyItem));
+            certifiedCopyItem.itemOptions.deliveryTimescale = "same-day";
+            patchCertifiedCopyItemStub = sandbox.stub(apiClient, "patchCertifiedCopyItem")
+                .returns(Promise.resolve(certifiedCopyItem));
             
+            const resp = await chai.request(testApp)
+                .post(DELIVERY_OPTIONS_URL)
+                .set("Cookie", [`__SID=${SIGNED_IN_COOKIE}`])
+                .redirects(0)
+                .send({
+                    deliveryOptions: "same-day"
+                });
+
+            chai.expect(resp.status).to.equal(302);
+            chai.expect(resp.text).to.include("Found. Redirecting to delivery-details");
+        });
+    });
+
+    describe("delivery options back button", () => {
+        it("back button takes the user to the certificate options page if they have not selected any options", async () => {
             getCertifiedCopyItemStub = sandbox.stub(apiClient, "getCertifiedCopyItem")
                 .returns(Promise.resolve(certifiedCopyItem));
 
             const resp = await chai.request(testApp)
                 .get(DELIVERY_OPTIONS_URL)
-                .set("Cookie", [`__SID=${SIGNED_IN_COOKIE}`])
-                .redirects(0)
-                .send();
+                .set("Cookie", [`__SID=${SIGNED_IN_COOKIE}`]);
+
+            const $ = cheerio.load(resp.text);
 
             chai.expect(resp.status).to.equal(200);
-            chai.expect(resp.text).to.contain(DELIVERY_OPTION_NOT_SELECTED);
+            chai.expect($(".govuk-back-link").attr("href")).to.include("certified-documents");
         });
     });
 
