51c1bef68b42cf9bb5ad0b6fa5f7d5947ad96bf2
hwilliams1 <hwilliams1@companieshouse.gov.uk>
2022-08-08 14:25:53 +0100

    Work in progress on unit tests

--- test/controller/certified-copies/delivery.options.controller.integration.test.ts
+++ test/controller/certified-copies/delivery.options.controller.integration.test.ts
@@ -4,26 +4,19 @@ import ioredis from "ioredis";
 import cheerio from "cheerio";
 import { SIGNED_IN_COOKIE, signedInSession } from "../../__mocks__/redis.mocks";
 import * as apiClient from "../../../src/client/api.client";
-import { Basket, ItemUriRequest } from "@companieshouse/api-sdk-node/dist/services/order/basket/types";
-import {
-    CERTIFIED_COPY_DELIVERY_OPTIONS,
-    replaceCertifiedCopyId,
-    
-} from "../../../src/model/page.urls";
 import { CertifiedCopyItem } from "@companieshouse/api-sdk-node/dist/services/order/certified-copies/types";
+import { CERTIFIED_COPY_DELIVERY_OPTIONS, replaceCertifiedCopyId } from "../../../src/model/page.urls";
+import { ItemOptions } from "@companieshouse/api-sdk-node/dist/services/order/order";
 
-const CERTIFIED_COPY_ID = "CCD-000000-000000";
-const DELIVERY_OPTION_NOT_SELECTED = "Select a delivery option";
-const DELIVERY_OPTIONS_URL =
-    replaceCertifiedCopyId(CERTIFIED_COPY_DELIVERY_OPTIONS, CERTIFIED_COPY_ID);
 const sandbox = sinon.createSandbox();
 let testApp = null;
 let getCertifiedCopyItemStub;
 let patchCertifiedCopyItemStub;
-let getBasketStub;
-let appendItemToBasketStub;
+const CERTIFIED_COPY_ID = "CCD-123456-123456";
+const DELIVERY_OPTION_NOT_SELECTED = "Select a delivery option";
+const DELIVERY_OPTIONS_URL = replaceCertifiedCopyId(CERTIFIED_COPY_DELIVERY_OPTIONS, CERTIFIED_COPY_ID);
 
-describe("delivery.options.integration.test", () => {
+describe("delivery.options.controller.integration.test", () => {
     beforeEach((done) => {
         sandbox.stub(ioredis.prototype, "connect").returns(Promise.resolve());
         sandbox.stub(ioredis.prototype, "get").returns(Promise.resolve(signedInSession));
@@ -61,12 +54,13 @@ describe("delivery.options.integration.test", () => {
     });
 
     describe("delivery options validation", () => {
-        it("throws a validation error when no option selected", async () => {
+        it.only("throws a validation error when no option selected", async () => {
+            
             getCertifiedCopyItemStub = sandbox.stub(apiClient, "getCertifiedCopyItem")
                 .returns(Promise.resolve(certifiedCopyItem));
 
             const resp = await chai.request(testApp)
-                .post(DELIVERY_OPTIONS_URL)
+                .get(DELIVERY_OPTIONS_URL)
                 .set("Cookie", [`__SID=${SIGNED_IN_COOKIE}`])
                 .redirects(0)
                 .send();
@@ -76,101 +70,4 @@ describe("delivery.options.integration.test", () => {
         });
     });
 
-    describe("delivery options patch", () => {
-        it("redirects the user to the delivery-details page", async () => {
-            const certifiedCopyDetails = {
-                itemOptions: {
-                    deliveryTimescale: "standard"
-                }
-            } as CertifiedCopyItem;
-
-            getCertifiedCopyItemStub = sandbox.stub(apiClient, "getCertifiedCopyItem")
-                .returns(Promise.resolve(certifiedCopyDetails));
-            patchCertifiedCopyItemStub = sandbox.stub(apiClient, "patchCertifiedCopyItem")
-                .returns(Promise.resolve(certifiedCopyDetails));
-            getBasketStub = sandbox.stub(apiClient, "getBasket")
-                .returns(Promise.resolve({ enrolled: false }));
-
-
-            const resp = await chai.request(testApp)
-                .post(DELIVERY_OPTIONS_URL)
-                .set("Cookie", [`__SID=${SIGNED_IN_COOKIE}`])
-                .redirects(0)
-                .send({
-                    deliveryOptions: "standard"
-                });
-
-            chai.expect(resp.status).to.equal(302);
-            chai.expect(resp.text).to.include("Found. Redirecting to delivery-details");
-        });
-
-        it("adds item to basket and redirects user to the basket page if enrolled", async () => {
-            const certifiedCopyDetails = {
-                itemOptions: {
-                    deliveryTimescale: "standard"
-                },
-                links: {
-                    self: "/path/to/certifiedCopy"
-                },
-                kind: "item#certifiedCopy"
-            } as CertifiedCopyItem;
-
-            getCertifiedCopyItemStub = sandbox.stub(apiClient, "getCertifiedCopyItem")
-                .returns(Promise.resolve(certifiedCopyDetails));
-            patchCertifiedCopyItemStub = sandbox.stub(apiClient, "patchCertificateItem")
-                .returns(Promise.resolve(certifiedCopyDetails));
-            getBasketStub = sandbox.stub(apiClient, "getBasket")
-                .returns(Promise.resolve({ enrolled: true, items: [{ kind: "item#certifiedCopy" } as any] }));
-            sandbox.mock(apiClient).expects("appendItemToBasket")
-                .once()
-                .returns(Promise.resolve());
-
-            const resp = await chai.request(testApp)
-                .post(DELIVERY_OPTIONS_URL)
-                .set("Cookie", [`__SID=${SIGNED_IN_COOKIE}`])
-                .redirects(0)
-                .send({
-                    deliveryOptions: "standard"
-                });
-
-            chai.expect(resp.status).to.equal(302);
-            chai.expect(resp.text).to.include("Found. Redirecting to /basket");
-        });
-    });
-
-    describe("delivery options back button", () => {
-        it("back button takes the user to the certified document page", async () => {
-            getCertifiedCopyItemStub = sandbox.stub(apiClient, "getCertifiedCopyItem")
-                .returns(Promise.resolve(certifiedCopyItem));
-
-            const resp = await chai.request(testApp)
-                .get(DELIVERY_OPTIONS_URL)
-                .set("Cookie", [`__SID=${SIGNED_IN_COOKIE}`]);
-
-            const $ = cheerio.load(resp.text);
-
-            chai.expect(resp.status).to.equal(200);
-            chai.expect($(".govuk-back-link").attr("href")).to.include("certified-documents");
-        });
-    });
-
-    describe("delivery option checked", () => {
-        it("displays checked option", async () => {
-            const certifiedCopyDetails = {
-                itemOptions: {
-                    deliveryTimescale: "same-day"
-                }
-            } as CertifiedCopyItem;
-
-            getCertifiedCopyItemStub = sandbox.stub(apiClient, "getCertifiedCopyItem")
-                .returns(Promise.resolve(certifiedCopyDetails));
-
-            const resp = await chai.request(testApp)
-                .get(DELIVERY_OPTIONS_URL)
-                .set("Cookie", [`__SID=${SIGNED_IN_COOKIE}`]);
-
-            chai.expect(resp.status).to.equal(200);
-            chai.expect(resp.text).to.include(`<input class="govuk-radios__input" id="deliveryOptions" name="deliveryOptions" type="radio" value="same-day" checked aria-describedby="deliveryOptions-item-hint" data-event-id="express-delivery">`);
-        });
-    });
 });
