49bb1d91bc3e39f3cf8bd5b3455672b496a818ab
dgroves-ch <dgroves@companieshouse.gov.uk>
2022-08-15 12:17:17 +0100

    Append certified copy to basket if enrolled
    * Append certified copy item to basket if user is enrolled.
    * User agent redirected to delivery details page if no deliverable items
      already in basket otherwise redirected to basket page.

--- src/controllers/certified-copies/delivery.options.controller.ts
+++ src/controllers/certified-copies/delivery.options.controller.ts
@@ -1,13 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { check, validationResult } from "express-validator";
-import { CertifiedCopyItem, CertifiedCopyItemPatchRequest} from "@companieshouse/api-sdk-node/dist/services/order/certified-copies/types";
+import { CertifiedCopyItem, CertifiedCopyItemPatchRequest } from "@companieshouse/api-sdk-node/dist/services/order/certified-copies/types";
 import { getAccessToken, getUserId } from "../../session/helper";
-import { getCertifiedCopyItem, patchCertifiedCopyItem } from "../../client/api.client";
+import { appendItemToBasket, getBasket, getCertifiedCopyItem, patchCertifiedCopyItem } from "../../client/api.client";
 import { DELIVERY_DETAILS, DELIVERY_OPTIONS } from "../../model/template.paths";
 import { createLogger } from "ch-structured-logging";
 import { APPLICATION_NAME, DISPATCH_DAYS } from "../../config/config";
 import { DELIVERY_OPTION_SELECTION } from "../../model/error.messages";
 import { createGovUkErrorData } from "../../model/govuk.error.data";
+import { BY_ITEM_KIND, StaticRedirectCallback } from "../certificates/StaticRedirectCallback";
 
 const PAGE_TITLE: string = "Delivery options - Order a certified document - GOV.UK";
 const logger = createLogger(APPLICATION_NAME);
@@ -15,6 +16,8 @@ const logger = createLogger(APPLICATION_NAME);
 const validators = [
     check("deliveryOptions").not().isEmpty().withMessage(DELIVERY_OPTION_SELECTION)
 ];
+
+const redirectCallback = new StaticRedirectCallback(BY_ITEM_KIND);
 
 export const render = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
@@ -68,15 +71,27 @@ const route = async (req: Request, res: Response, next: NextFunction) => {
                 errorList: [deliveryOptionsErrorData]
             });
         } else {
-            let certifiedCopyItemPatchRequest: CertifiedCopyItemPatchRequest;
-            certifiedCopyItemPatchRequest = {
+            const certifiedCopyItemPatchRequest = {
                 itemOptions: {
                     deliveryTimescale: req.body.deliveryOptions
                 }
             };
             const patchedCertifiedCopyItem = await patchCertifiedCopyItem(accessToken, req.params.certifiedCopyId, certifiedCopyItemPatchRequest);
             logger.info(`Patched certified copy item with delivery option, id=${req.params.certifiedCopyId}, user_id=${userId}, company_number=${patchedCertifiedCopyItem.companyNumber}`);
-            return res.redirect(DELIVERY_DETAILS);
+            const basket = await getBasket(accessToken);
+            if (basket.enrolled) {
+                logger.debug(`User ${userId} is enrolled; appending item ${req.params.certifiedCopyId} to basket...`);
+                await appendItemToBasket(accessToken, {
+                    itemUri: patchedCertifiedCopyItem.links.self
+                });
+                return redirectCallback.redirectEnrolled({
+                    response: res,
+                    items: basket.items
+                });
+            } else {
+                logger.debug(`User ${userId} is disenrolled; redirecting user to delivery details page...`);
+                return res.redirect(DELIVERY_DETAILS);
+            }
         }
     } catch (err) {
         logger.error(`${err}`);
