289b4f6c19f32f6e405e2d54672409e797d09d3a
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2022-08-16 10:19:37 +0100

    BI-11519: jQuery $ allows click() callback to be set correctly?

--- static/js/mobile-menu.js
+++ static/js/mobile-menu.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     document.querySelector("ul#navigation").classList.add("mobile-hidden");
-    document.querySelector(".js-toggle-nav.js-header-toggle").click(() => {
+    $(".js-toggle-nav .js-header-toggle").click(() => {
         event.preventDefault();
         document.querySelector("ul#navigation").classList.toggle("mobile-hidden");
         document.querySelector("#global-nav").classList.toggle("open");
