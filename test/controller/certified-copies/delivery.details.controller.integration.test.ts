1c10dab344c9d7278cc0c95273f5e22acb32028d
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2022-08-30 16:15:04 +0100

    BI-11579: Avoid failing non-existent field for cert copies.

--- test/controller/certified-copies/delivery.details.controller.integration.test.ts
+++ test/controller/certified-copies/delivery.details.controller.integration.test.ts
@@ -196,7 +196,6 @@ describe("certified.copies.delivery.details.controller", () => {
                     addressLineTwo: "Pontcanna",
                     addressPostcode: "CF11 9VE",
                     addressTown: "CARDIFF",
-                    companyName: "Ink Inc",
                     firstName: "JOHN",
                     lastName: "SMITH"
                 })
