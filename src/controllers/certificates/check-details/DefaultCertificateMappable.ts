3d166ed2d6e49c353a03732e890ba3404a548383
aokunade <aokunade@companieshouse.gov.uk>
2022-10-21 08:03:08 +0100

    changed variable name previously omitted

--- src/controllers/certificates/check-details/DefaultCertificateMappable.ts
+++ src/controllers/certificates/check-details/DefaultCertificateMappable.ts
@@ -6,6 +6,6 @@ export interface DefaultCertificateMappable {
     prependCurrencySymbol (fee: string): string
     mapAddressOption (addressOption: string | undefined): string
     mapDeliveryDetails (deliveryDetails: DeliveryDetails | undefined): string
-    mapDeliveryMethod (itemOptions: Record<string, any>, basket:Boolean): string | null
+    mapDeliveryMethod (itemOptions: Record<string, any>, multiBasketEnabled:Boolean): string | null
     mapEmailCopyRequired (itemOptions: Record<string, any>): string
 }
