fd99d54c87b3e5d6b1b0b81ffc191ed55dc24d01
caismith-ch <csmith2@companieshouse.gov.uk>
2022-11-02 10:43:30 +0000

    Initial implementation of basket link on single page

---
+++ src/utils/basket.utils.ts
@@ -0,0 +1,20 @@
+import { Request } from "express";
+import { SessionKey } from "@companieshouse/node-session-handler/lib/session/keys/SessionKey";
+import { SignInInfoKeys } from "@companieshouse/node-session-handler/lib/session/keys/SignInInfoKeys";
+import { Basket } from "@companieshouse/api-sdk-node/dist/services/order/basket";
+import { getBasket } from "../client/api.client";
+import { BASKET_WEB_URL } from "../config/config";
+import { BasketLink } from "../model/BasketLink";
+
+export const getBasketLink = async (req: Request) : Promise<BasketLink> => {
+    const signedIn = req.session?.data?.[SessionKey.SignInInfo]?.[SignInInfoKeys.SignedIn] === 1;
+    if (!signedIn) {
+        return { showBasketLink: false };
+    }
+    const signInInfo = req.session?.data[SessionKey.SignInInfo];
+    const accessToken = signInInfo?.[SignInInfoKeys.AccessToken]?.[SignInInfoKeys.AccessToken]!;
+
+    const basket: Basket = await getBasket(accessToken);
+
+    return { showBasketLink: basket.enrolled, basketWebUrl: BASKET_WEB_URL, basketItems: basket.items?.length };
+};
