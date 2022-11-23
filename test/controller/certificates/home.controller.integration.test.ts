a0984acc1406f2e7a5e430ac9b69f1ff8b62fcf1
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2022-11-23 12:19:43 +0000

    BI-12133: Present warning in box if item limit reached or exceeded.

--- test/controller/certificates/home.controller.integration.test.ts
+++ test/controller/certificates/home.controller.integration.test.ts
@@ -14,6 +14,10 @@ import {
 } from "../../__mocks__/certificates.mocks";
 import { FEATURE_FLAGS } from "../../../src/config/FeatureFlags";
 import cheerio from "cheerio";
+import { BASKET_ITEM_LIMIT } from "../../../src/config/config";
+import { getDummyBasket } from "../../utils/basket.utils.test";
+import { SIGNED_IN_COOKIE, signedInSession } from "../../__mocks__/redis.mocks";
+import * as apiClient from "../../../src/client/api.client";
 
 const COMPANY_NUMBER = "00000000";
 
@@ -23,7 +27,8 @@ let getCompanyProfileStub;
 
 describe("certificate.home.controller.integration", () => {
     beforeEach((done) => {
-        sandbox.stub(ioredis.prototype, "connect").returns(Promise.resolve());
+        sandbox.stub(ioredis.prototype, "connect").resolves();
+        sandbox.stub(ioredis.prototype, "get").resolves(signedInSession);
 
         testApp = require("../../../src/app").default;
         done();
@@ -36,7 +41,7 @@ describe("certificate.home.controller.integration", () => {
 
     it("renders the start page if company type is allowed to order a certificate", async () => {
         getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
-            .returns(Promise.resolve(dummyCompanyProfileAcceptableCompanyType));
+            .resolves(dummyCompanyProfileAcceptableCompanyType);
 
         const resp = await chai.request(testApp)
             .get(replaceCompanyNumber(ROOT_CERTIFICATE, COMPANY_NUMBER));
@@ -47,7 +52,7 @@ describe("certificate.home.controller.integration", () => {
 
     it("displays the notification banner with the in context company name and company number", async () => {
         getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
-            .returns(Promise.resolve(dummyCompanyProfileAcceptableCompanyType));
+            .resolves(dummyCompanyProfileAcceptableCompanyType);
 
         const resp = await chai.request(testApp)
             .get(replaceCompanyNumber(ROOT_CERTIFICATE, COMPANY_NUMBER));
@@ -58,7 +63,7 @@ describe("certificate.home.controller.integration", () => {
 
     it("does not render the start page if company type is not allowed to order a certificate", async () => {
         getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
-            .returns(Promise.resolve(dummyCompanyProfileNotAcceptableCompanyType));
+            .resolves(dummyCompanyProfileNotAcceptableCompanyType);
 
         const resp = await chai.request(testApp)
             .get(replaceCompanyNumber(ROOT_CERTIFICATE, COMPANY_NUMBER));
@@ -69,7 +74,7 @@ describe("certificate.home.controller.integration", () => {
 
     it("renders the start page for dissolved company type that is allowed to order a certificate", async () => {
         getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
-            .returns(Promise.resolve(mockAcceptableDissolvedCompanyProfile));
+            .resolves(mockAcceptableDissolvedCompanyProfile);
 
         const resp = await chai.request(testApp)
             .get(replaceCompanyNumber(ROOT_CERTIFICATE, mockCompanyProfileConfiguration.companyNumber));
@@ -85,7 +90,7 @@ describe("certificate.home.controller.integration", () => {
 
     it("renders the start page for allowed active company to order a certificate", async () => {
         getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
-            .returns(Promise.resolve(getMockCompanyProfile()));
+            .resolves(getMockCompanyProfile());
 
         const resp = await chai.request(testApp)
             .get(replaceCompanyNumber(ROOT_CERTIFICATE, mockCompanyProfileConfiguration.companyNumber));
@@ -103,7 +108,7 @@ describe("certificate.home.controller.integration", () => {
 
     it("renders the start page for active llp company to order a certificate", async () => {
         getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
-            .returns(Promise.resolve(getMockCompanyProfile({ companyType: "llp", companyStatus: "active" })));
+            .resolves(getMockCompanyProfile({ companyType: "llp", companyStatus: "active" }));
 
         const resp = await chai.request(testApp)
             .get(replaceCompanyNumber(ROOT_CERTIFICATE, mockCompanyProfileConfiguration.companyNumber));
@@ -121,7 +126,7 @@ describe("certificate.home.controller.integration", () => {
     it("renders the start page for limited company in liquidation to order a certificate if feature flag enabled", async () => {
         FEATURE_FLAGS.liquidatedCompanyCertificateEnabled = true;
         getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
-            .returns(Promise.resolve(getMockCompanyProfile({ companyType: "ltd", companyStatus: "liquidation" })));
+            .resolves(getMockCompanyProfile({ companyType: "ltd", companyStatus: "liquidation" }));
 
         const resp = await chai.request(testApp)
             .get(replaceCompanyNumber(ROOT_CERTIFICATE, mockCompanyProfileConfiguration.companyNumber));
@@ -140,7 +145,7 @@ describe("certificate.home.controller.integration", () => {
     it("renders the start page for an llp company in liquidation to order a certificate if feature flag enabled", async () => {
         FEATURE_FLAGS.liquidatedCompanyCertificateEnabled = true;
         getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
-            .returns(Promise.resolve(getMockCompanyProfile({ companyType: "llp", companyStatus: "liquidation" })));
+            .resolves(getMockCompanyProfile({ companyType: "llp", companyStatus: "liquidation" }));
 
         const resp = await chai.request(testApp)
             .get(replaceCompanyNumber(ROOT_CERTIFICATE, mockCompanyProfileConfiguration.companyNumber));
@@ -158,7 +163,7 @@ describe("certificate.home.controller.integration", () => {
     it("does not render the start page for company in liquidation if feature flag disabled", async () => {
         FEATURE_FLAGS.liquidatedCompanyCertificateEnabled = false;
         getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
-            .returns(Promise.resolve(getMockCompanyProfile({ companyType: "ltd", companyStatus: "liquidation" })));
+            .resolves(getMockCompanyProfile({ companyType: "ltd", companyStatus: "liquidation" }));
 
         const resp = await chai.request(testApp)
             .get(replaceCompanyNumber(ROOT_CERTIFICATE, mockCompanyProfileConfiguration.companyNumber));
@@ -169,7 +174,7 @@ describe("certificate.home.controller.integration", () => {
 
     it("does not render the start page for dissolved company type that is not allowed to order a certificate", async () => {
         getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
-            .returns(Promise.resolve(mockNotAcceptableDissolvedCompanyLimitedPartnershipProfile));
+            .resolves(mockNotAcceptableDissolvedCompanyLimitedPartnershipProfile);
 
         const resp = await chai.request(testApp)
             .get(replaceCompanyNumber(ROOT_CERTIFICATE, mockCompanyProfileConfiguration.companyNumber));
@@ -181,7 +186,7 @@ describe("certificate.home.controller.integration", () => {
     it("renders the start page for limited company in administration to order a certificate if feature flag enabled", async () => {
         FEATURE_FLAGS.administrationCompanyCertificateEnabled = true;
         getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
-            .returns(Promise.resolve(getMockCompanyProfile({ companyType: "ltd", companyStatus: "administration" })));
+            .resolves(getMockCompanyProfile({ companyType: "ltd", companyStatus: "administration" }));
 
         const resp = await chai.request(testApp)
             .get(replaceCompanyNumber(ROOT_CERTIFICATE, mockCompanyProfileConfiguration.companyNumber));
@@ -200,7 +205,7 @@ describe("certificate.home.controller.integration", () => {
     it("renders the start page for an llp company in administration to order a certificate if feature flag enabled", async () => {
         FEATURE_FLAGS.administrationCompanyCertificateEnabled = true;
         getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
-            .returns(Promise.resolve(getMockCompanyProfile({ companyType: "llp", companyStatus: "administration" })));
+            .resolves(getMockCompanyProfile({ companyType: "llp", companyStatus: "administration" }));
 
         const resp = await chai.request(testApp)
             .get(replaceCompanyNumber(ROOT_CERTIFICATE, mockCompanyProfileConfiguration.companyNumber));
@@ -218,7 +223,7 @@ describe("certificate.home.controller.integration", () => {
     it("does not render the start page for company in administration if feature flag disabled", async () => {
         FEATURE_FLAGS.administrationCompanyCertificateEnabled = false;
         getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
-            .returns(Promise.resolve(getMockCompanyProfile({ companyType: "ltd", companyStatus: "administration" })));
+            .resolves(getMockCompanyProfile({ companyType: "ltd", companyStatus: "administration" }));
 
         const resp = await chai.request(testApp)
             .get(replaceCompanyNumber(ROOT_CERTIFICATE, mockCompanyProfileConfiguration.companyNumber));
@@ -229,7 +234,7 @@ describe("certificate.home.controller.integration", () => {
 
     it("renders the correct start now button URL for an active company", async () => {
         getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
-            .returns(Promise.resolve(dummyCompanyProfileAcceptableCompanyType));
+            .resolves(dummyCompanyProfileAcceptableCompanyType);
 
         const resp = await chai.request(testApp)
             .get(replaceCompanyNumber(ROOT_CERTIFICATE, COMPANY_NUMBER));
@@ -242,7 +247,7 @@ describe("certificate.home.controller.integration", () => {
 
     it("renders the correct start now button URL for a dissolved company", async () => {
         getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
-            .returns(Promise.resolve(mockAcceptableDissolvedCompanyProfile));
+            .resolves(mockAcceptableDissolvedCompanyProfile);
 
         const resp = await chai.request(testApp)
             .get(replaceCompanyNumber(ROOT_CERTIFICATE, mockCompanyProfileConfiguration.companyNumber));
@@ -251,5 +256,61 @@ describe("certificate.home.controller.integration", () => {
 
         const $ = cheerio.load(resp.text);
         chai.expect($(".govuk-button--start").attr("href")).to.equal("/company/00000000/orderable/dissolved-certificates/certificate-type");
+    });
+
+    it("renders `This order will be for...` message when no basket link is shown", async () => {
+        getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
+            .resolves(mockAcceptableDissolvedCompanyProfile);
+
+        const resp = await chai.request(testApp)
+            .get(replaceCompanyNumber(ROOT_CERTIFICATE, mockCompanyProfileConfiguration.companyNumber));
+
+        chai.expect(resp.status).to.equal(200);
+        chai.expect(resp.text).to.contain("Sign in / Register");
+        chai.expect(resp.text).to.contain("This order will be for company name (00000000)");
+    });
+
+    it("renders `This order will be for...` message when items below the limit", async () => {
+        getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
+            .resolves(mockAcceptableDissolvedCompanyProfile);
+        sandbox.stub(apiClient, "getBasket").resolves(getDummyBasket(true, BASKET_ITEM_LIMIT - 1));
+
+        const resp = await chai.request(testApp)
+            .get(replaceCompanyNumber(ROOT_CERTIFICATE, mockCompanyProfileConfiguration.companyNumber))
+            .set("Cookie", [`__SID=${SIGNED_IN_COOKIE}`]);
+
+        chai.expect(resp.status).to.equal(200);
+        chai.expect(resp.text).to.contain(`Basket (${BASKET_ITEM_LIMIT - 1})`);
+        chai.expect(resp.text).to.contain("This order will be for company name (00000000)");
+    });
+
+    it("renders `Your basket is full...` warning when items at the limit", async () => {
+        getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
+            .resolves(mockAcceptableDissolvedCompanyProfile);
+        sandbox.stub(apiClient, "getBasket").resolves(getDummyBasket(true, BASKET_ITEM_LIMIT));
+
+        const resp = await chai.request(testApp)
+            .get(replaceCompanyNumber(ROOT_CERTIFICATE, mockCompanyProfileConfiguration.companyNumber))
+            .set("Cookie", [`__SID=${SIGNED_IN_COOKIE}`]);
+
+        chai.expect(resp.status).to.equal(200);
+        chai.expect(resp.text).to.contain(`Basket (${BASKET_ITEM_LIMIT})`);
+        chai.expect(resp.text).to.contain(
+            `Your basket is full. You cannot add more than ${BASKET_ITEM_LIMIT} items to your order.`);
+    });
+
+    it("renders `Your basket is full...` warning when items over the limit", async () => {
+        getCompanyProfileStub = sandbox.stub(CompanyProfileService.prototype, "getCompanyProfile")
+            .resolves(mockAcceptableDissolvedCompanyProfile);
+        sandbox.stub(apiClient, "getBasket").resolves(getDummyBasket(true, BASKET_ITEM_LIMIT + 1));
+
+        const resp = await chai.request(testApp)
+            .get(replaceCompanyNumber(ROOT_CERTIFICATE, mockCompanyProfileConfiguration.companyNumber))
+            .set("Cookie", [`__SID=${SIGNED_IN_COOKIE}`]);
+
+        chai.expect(resp.status).to.equal(200);
+        chai.expect(resp.text).to.contain(`Basket (${BASKET_ITEM_LIMIT + 1})`);
+        chai.expect(resp.text).to.contain(
+            `Your basket is full. You cannot add more than ${BASKET_ITEM_LIMIT} items to your order.`);
     });
 });
