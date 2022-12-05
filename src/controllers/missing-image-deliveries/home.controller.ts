738bbfd76625852b1c41fb06551cb3af1e45bec3
caismith-ch <csmith2@companieshouse.gov.uk>
2022-12-05 12:06:00 +0000

    Add warning message for a full basket at start of mid journey

--- src/controllers/missing-image-deliveries/home.controller.ts
+++ src/controllers/missing-image-deliveries/home.controller.ts
@@ -4,8 +4,9 @@ import { MISSING_IMAGE_DELIVERY_CREATE, replaceCompanyNumberAndFilingHistoryId }
 import { CompanyProfile } from "@companieshouse/api-sdk-node/dist/services/company-profile";
 import { getCompanyProfile } from "../../client/api.client";
 import {API_KEY, APPLICATION_NAME} from "../../config/config";
-import { getBasketLink } from "../../utils/basket.utils";
+import { getBasketLimit, getBasketLink } from "../../utils/basket.utils";
 import { BasketLink } from "../../model/BasketLink";
+import { BasketLimit, BasketLimitState } from "../../model/BasketLimit";
 import {createLogger} from "ch-structured-logging";
 
 const logger = createLogger(APPLICATION_NAME);
@@ -19,8 +20,17 @@ export default async (req: Request, res: Response, next: NextFunction) => {
         const startNowUrl: string = replaceCompanyNumberAndFilingHistoryId(MISSING_IMAGE_DELIVERY_CREATE, companyNumber, filingHistoryId);
         const SERVICE_URL = `/company/${companyNumber}/filing-history`;
         const basketLink: BasketLink = await getBasketLink(req);
+        const basketLimit: BasketLimit = getBasketLimit(basketLink);
 
-        res.render(MISSING_IMAGE_DELIVERY_INDEX, {companyNumber, startNowUrl, SERVICE_URL, companyName, ...basketLink});
+        res.render(MISSING_IMAGE_DELIVERY_INDEX,
+            {
+                companyNumber,
+                startNowUrl,
+                SERVICE_URL,
+                companyName,
+                ...basketLink,
+                ...basketLimit
+            });
     } catch (err) {
         logger.error(`${err}`);
         next(err);
