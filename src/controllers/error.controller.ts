3680b97ebeef41ba61ecc04700b9d316c9a69d74
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-01-10 17:25:59 +0000

    BI-12337: Get and render page header info in error controller too.

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -3,17 +3,17 @@ import { HttpError } from "http-errors";
 import { APPLICATION_NAME, CHS_URL } from "../config/config";
 import { createLogger } from "ch-structured-logging";
 import * as templatePaths from "../model/template.paths";
+import { PageHeader } from "../model/PageHeader";
+import { mapPageHeader } from "../utils/page.header.utils";
 
 const logger = createLogger(APPLICATION_NAME);
-const serviceName = "Find and update company information";
 const SERVICE_URL = CHS_URL;
 
 const errorHandler = (err: unknown, req: Request, res: Response, next: NextFunction) => {
-    const signedIn = req.session?.data.signin_info?.signed_in;
-    const userEmail = req.session?.data?.signin_info?.user_profile?.email;
     const errorStatusCode = err instanceof HttpError ? err?.statusCode : 500;
     logger.error("Error: " + `${req.path}`);
-    res.status(errorStatusCode).render(templatePaths.ERROR, { errorMessage: err, signedIn, userEmail, serviceName, SERVICE_URL });
+    const pageHeader: PageHeader = mapPageHeader(req);
+    res.status(errorStatusCode).render(templatePaths.ERROR, { errorMessage: err, ...pageHeader, SERVICE_URL });
 };
 
 export default errorHandler;
