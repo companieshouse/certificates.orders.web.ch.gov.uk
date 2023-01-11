184038418455ca3584f1e1ef0c4d95a6d3f7a8ee
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-01-11 09:09:14 +0000

    BI-12337: Revert to hard-wiring service name for simplicity.

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -7,12 +7,14 @@ import { PageHeader } from "../model/PageHeader";
 import { mapPageHeader } from "../utils/page.header.utils";
 
 const logger = createLogger(APPLICATION_NAME);
+const serviceName = "Find and update company information";
 const SERVICE_URL = CHS_URL;
 
 const errorHandler = (err: unknown, req: Request, res: Response, next: NextFunction) => {
     const errorStatusCode = err instanceof HttpError ? err?.statusCode : 500;
     logger.error("Error: " + `${req.path}`);
     const pageHeader: PageHeader = mapPageHeader(req);
+    pageHeader.serviceName = serviceName;
     res.status(errorStatusCode).render(templatePaths.ERROR, { errorMessage: err, ...pageHeader, SERVICE_URL });
 };
 
