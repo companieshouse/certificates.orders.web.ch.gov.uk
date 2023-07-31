41760aab38cffcd0223efe06698b5d522ed5b3f6
caismith-ch <csmith2@companieshouse.gov.uk>
2023-07-31 18:32:06 +0100

    Fix broken unit/intergration tests

--- src/config/api.enumerations.ts
+++ src/config/api.enumerations.ts
@@ -4,10 +4,10 @@ import yaml from "js-yaml";
 const FILING_HISTORY_DESCRIPTIONS_PATH: string = "api-enumerations/filing_history_descriptions.yml";
 const DESCRIPTIONS_CONSTANT: string = "description";
 
-const filingHistoryDescriptions = yaml.safeLoad(fs.readFileSync(FILING_HISTORY_DESCRIPTIONS_PATH, "utf8"));
+const filingHistoryDescriptions = yaml.load(fs.readFileSync(FILING_HISTORY_DESCRIPTIONS_PATH, "utf8"));
 
 export const getFullFilingHistoryDescription = (descriptionKey: string): string => {
-    if (filingHistoryDescriptions === undefined) {
+    if (filingHistoryDescriptions === undefined || filingHistoryDescriptions === null) {
         return descriptionKey;
     } else {
         return filingHistoryDescriptions[DESCRIPTIONS_CONSTANT][descriptionKey] || descriptionKey;
