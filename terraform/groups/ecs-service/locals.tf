9035839590a511e850b848b80181faba91c7da1b
Lee Seviour <lseviour@companieshouse.gov.uk>
2023-08-22 09:19:04 +0100

    Update LB paths

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -7,22 +7,11 @@ locals {
   docker_repo               = "certificates.orders.web.ch.gov.uk"
   lb_listener_rule_priority = 11
   lb_listener_paths         = [
-    "/company/*/orderable/certificates",
-    "/company/*/orderable/dissolved-certificates",
-    "/company/*/orderable/certified-copies",
-    "/company/*/orderable/missing-image-deliveries",
-    "/orderable/certificates",
-    "/orderable/dissolved-certificates",
-    "/orderable/certified-copies",
-    "/orderable/missing-image-deliveries",
-    "/orderable/certificates-assets",
-    "/company/*/orderable/lp-certificates",
-    "/orderable/lp-certificates",
-    "/company/*/orderable/llp-certificates",
-    "/orderable/llp-certificates"
+    "/company/*/orderable/*",
+    "/orderable/*"
   ]
   healthcheck_path          = "/orderable/certificates" #healthcheck path for certificates orders web
-  healthcheck_matcher       = "200-302"              # no explicit healthcheck in this service yet, change this when added!
+  healthcheck_matcher       = "200-302"                 # no explicit healthcheck in this service yet, change this when added!
 
   service_secrets = jsondecode(data.vault_generic_secret.service_secrets.data_json)
 
