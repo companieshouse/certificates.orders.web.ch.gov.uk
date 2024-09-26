b0fbb9b89f64f63bc7cd6efc7fffc42cebaec62d
tombradley1 <tbradley@companieshouse.gov.uk>
2024-09-26 09:00:33 +0100

    add cookie_name env var

--- test/setup.ts
+++ test/setup.ts
@@ -10,6 +10,7 @@ chai.use(chaiAsPromised);
 process.env.PIWIK_URL = "test";
 process.env.PIWIK_SITE_ID = "test";
 process.env.COOKIE_SECRET = "Xy6onkjQWF0TkRn0hfdqUw==";
+process.env.COOKIE_NAME = "test";
 process.env.COOKIE_DOMAIN = "cookie domain";
 process.env.CACHE_SERVER = "secret";
 process.env.CHS_API_KEY = "Xy6onkjQWF0TkRn0hfdq=";
